d4a833ff3ea9aabaaff8356f952aecdde8aaec96
Marco Paniconi <marpan@google.com>
2024-12-27 09:47:13 -0800

    rtc-screen: Make sb horiz scroll search window same as vert
    Add more conditions for increasing the horiz/col scroll
    motion window, to reduce speed regression. Condition
    it on the high motion frame-level flag and whether
    the last TL0 had horiz scroll.
    
    Leave the condition as is for now for the vert/row
    scroll since its more common.
    
    Bug: b:383306740
    Change-Id: Idf54694ef53f024da2e9a7561f25db77f9348bf0

--- av1/encoder/var_based_part.c
+++ av1/encoder/var_based_part.c
@@ -1424,8 +1424,11 @@ static void setup_planes(AV1_COMP *cpi, MACROBLOCK *x, unsigned int *y_sad,
         // threshold and motion level above LowSad.
         if (x->source_variance > 100 && source_sad_nonrd > kLowSad) {
           int is_screen = cpi->oxcf.tune_cfg.content == AOM_CONTENT_SCREEN;
+          int increase_col_sw = source_sad_nonrd > kMedSad &&
+              !cpi->rc.high_motion_content_screen_rtc &&
+              (cpi->svc.temporal_layer_id == 0 || cpi->rc.num_col_blscroll_last_tl0 > 2);
           int me_search_size_col =
-              is_screen ? source_sad_nonrd > kMedSad ? 160 : 96
+              is_screen ? increase_col_sw ? 512 : 96
                         : block_size_wide[cm->seq_params->sb_size] >> 1;
           // For screen use larger search size row motion to capture
           // vertical scroll, which can be larger motion.
@@ -1445,6 +1448,12 @@ static void setup_planes(AV1_COMP *cpi, MACROBLOCK *x, unsigned int *y_sad,
             if (*y_sad < (y_sad_zero >> 1) && *y_sad < thresh_sad) {
               x->sb_me_partition = 1;
               x->sb_me_mv.as_int = mi->mv[0].as_int;
+              if (cpi->svc.temporal_layer_id == 0) {
+                if (abs(mi->mv[0].as_mv.col) > 16 && abs(mi->mv[0].as_mv.row) == 0)
+                  cpi->rc.num_col_blscroll_last_tl0++;
+                else if (abs(mi->mv[0].as_mv.row) > 16 && abs(mi->mv[0].as_mv.col) == 0)
+                  cpi->rc.num_row_blscroll_last_tl0++;
+              }
             } else {
               x->sb_me_partition = 0;
               // Fall back to using zero motion.
