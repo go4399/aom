f8defebd99879e22d527c5ee35146d5ee9d0b281
Philipp Hancke <phancke@meta.com>
2025-01-09 15:14:19 -0800

    Allow per-frame calculation of PSNR
    See here for the libvpx change:
      https://chromium-review.googlesource.com/c/webm/libvpx/+/6167966
    and here for the WebRTC change with the intended usage:
      https://webrtc-review.googlesource.com/c/src/+/368960
    
    BUG=webrtc:388070060
    
    Change-Id: I50e82bfc9673133844fc438a4e204a88a4917f01

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -3268,8 +3268,16 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
   if (res == AOM_CODEC_OK) {
     AV1_COMP *cpi = ppi->cpi;
 
+    // Per-frame PSNR is not supported when g_lag_in_frames is greater than 0.
+    if ((flags & AOM_EFLAG_CALCULATE_PSNR) && ctx->cfg.g_lag_in_frames != 0) {
+      aom_internal_error(
+          &ppi->error, AOM_CODEC_INCAPABLE,
+          "Cannot calculate per-frame PSNR when g_lag_in_frames is nonzero");
+    }
+
     // Set up internal flags
-    if (ctx->base.init_flags & AOM_CODEC_USE_PSNR) ppi->b_calculate_psnr = 1;
+    ppi->b_calculate_psnr = (flags & AOM_EFLAG_CALCULATE_PSNR) ||
+                            (ctx->base.init_flags & AOM_CODEC_USE_PSNR);
 
     if (img != NULL) {
       if (!ctx->pts_offset_initialized) {
