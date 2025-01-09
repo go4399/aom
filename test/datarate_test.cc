5a79200c31b8f9cbe665a014ff0801e722f08d58
Philipp Hancke <phancke@meta.com>
2025-01-09 15:14:19 -0800

    Allow per-frame calculation of PSNR
    See here for the libvpx change:
      https://chromium-review.googlesource.com/c/webm/libvpx/+/6167966
    and here for the WebRTC change with the intended usage:
      https://webrtc-review.googlesource.com/c/src/+/368960
    
    BUG=webrtc:388070060
    
    Change-Id: I50e82bfc9673133844fc438a4e204a88a4917f01

--- test/datarate_test.cc
+++ test/datarate_test.cc
@@ -759,6 +759,48 @@ TEST_P(DatarateTestSetFrameQpRealtime, SetFrameQpOnePass) {
   ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
 }
 
+class DatarateTestPsnr
+    : public DatarateTest,
+      public ::testing::TestWithParam<const libaom_test::AV1CodecFactory *> {
+ public:
+  DatarateTestPsnr() : DatarateTest(GetParam()) {}
+
+ protected:
+  ~DatarateTestPsnr() override = default;
+
+  void SetUp() override {
+    InitializeConfig(libaom_test::kRealTime);
+    ResetModel();
+  }
+  void PreEncodeFrameHook(::libaom_test::VideoSource *video,
+                          ::libaom_test::Encoder *encoder) override {
+    DatarateTest::PreEncodeFrameHook(video, encoder);
+    frame_flags_ ^= AOM_EFLAG_CALCULATE_PSNR;
+  }
+  void PostEncodeFrameHook(::libaom_test::Encoder *encoder) override {
+    libaom_test::CxDataIterator iter = encoder->GetCxData();
+
+    bool had_psnr = false;
+    size_t packets = 0;
+    while (const aom_codec_cx_pkt_t *pkt = iter.Next()) {
+      packets++;
+      if (pkt->kind == AOM_CODEC_PSNR_PKT) had_psnr = true;
+    }
+    if (packets > 0) {
+      EXPECT_EQ(had_psnr, (frame_flags_ & AOM_EFLAG_CALCULATE_PSNR) ==
+                              AOM_EFLAG_CALCULATE_PSNR);
+    }
+  }
+};
+
+TEST_P(DatarateTestPsnr, PerFramePsnr) {
+  ::libaom_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
+                                       30, 1, 0, 100);
+
+  ResetModel();
+  ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
+}
+
 AV1_INSTANTIATE_TEST_SUITE(DatarateTestLarge,
                            ::testing::Values(::libaom_test::kRealTime),
                            ::testing::Range(5, 7), ::testing::Values(0, 3),
@@ -783,6 +825,11 @@ AV1_INSTANTIATE_TEST_SUITE(DatarateTestSpeedChangeRealtime,
 
 INSTANTIATE_TEST_SUITE_P(
     AV1, DatarateTestSetFrameQpRealtime,
+    ::testing::Values(
+        static_cast<const libaom_test::CodecFactory *>(&libaom_test::kAV1)));
+
+INSTANTIATE_TEST_SUITE_P(
+    AV1, DatarateTestPsnr,
     ::testing::Values(
         static_cast<const libaom_test::CodecFactory *>(&libaom_test::kAV1)));
 
