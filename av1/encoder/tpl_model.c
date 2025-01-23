e891f62d24b86bd2e98e2793b887717e9eab5c28
Wan-Teh Chang <wtc@google.com>
2025-01-23 15:32:04 -0800

    Rename tune=ssimulacra2 to tune=perceptual
    We want tune=ssimulacra2 to mean a mode that maximizes the SSIMULACRA 2
    metric. The current tune=ssimulacra2 mode increases image quality by
    using the SSIMULACRA 2 metric as a guidance and validating with
    subjective visual quality checks. Rename it tune=perceptual.
    
    Bug: b:390668380
    Change-Id: I6f1943395ffc186e9cc773e96d0d69c80d857641

--- av1/encoder/tpl_model.c
+++ av1/encoder/tpl_model.c
@@ -1377,8 +1377,8 @@ static inline void init_mc_flow_dispenser(AV1_COMP *cpi, int frame_idx,
       cpi->use_ducky_encode ? gf_group->q_val[frame_idx] : pframe_qindex;
   // The TPL model is only meant to be run in inter mode, so ensure that we are
   // not running in all intra mode, which implies we are not tuning for
-  // SSIMULACRA 2.
-  assert(cpi->oxcf.tune_cfg.tuning != AOM_TUNE_SSIMULACRA2 &&
+  // perceptual quality.
+  assert(cpi->oxcf.tune_cfg.tuning != AOM_TUNE_PERCEPTUAL &&
          cpi->oxcf.mode != ALLINTRA);
   // Get rd multiplier set up.
   rdmult = av1_compute_rd_mult(
