abe329612669633d2915b78f9ffbb33c6a8f30c5
Marco Paniconi <marpan@google.com>
2025-01-23 22:24:23 -0800

    rtc-screen: Speed feature for fast encoding on scene change
    To reduce encoding time spikes, as reported in the issue below.
    
    To be tested and tuned.
    
    This mode has quality loss so could be exposed as control for
    the user.
    
    Bug: b:391849810
    Change-Id: Iacab126149690642faead4aa2c72ea3621c411a6

--- av1/encoder/nonrd_pickmode.c
+++ av1/encoder/nonrd_pickmode.c
@@ -2432,6 +2432,12 @@ static AOM_FORCE_INLINE bool skip_inter_mode_nonrd(
     *ref_frame2 = NONE_FRAME;
   }
 
+  if (cpi->sf.rt_sf.fast_encoding_scene_change && cpi->rc.high_source_sad &&
+      x->content_state_sb.source_sad_nonrd >= kHighSad &&
+      bsize <= BLOCK_16X16 && !x->sb_me_block &&
+      (*ref_frame != LAST_FRAME || *this_mode == NEWMV))
+    return true;
+
   if (segfeature_active(&cm->seg, segment_id, SEG_LVL_SKIP) &&
       (*this_mode != GLOBALMV || *ref_frame != LAST_FRAME))
     return true;
@@ -3147,6 +3153,7 @@ static inline bool enable_palette(AV1_COMP *cpi, bool is_mode_intra,
                                   int force_zeromv_skip, int skip_idtx_palette,
                                   int force_palette_test,
                                   unsigned int best_intra_sad_norm) {
+  const unsigned int sad_thresh = 10;
   if (!cpi->oxcf.tool_cfg.enable_palette) return false;
   if (!av1_allow_palette(cpi->common.features.allow_screen_content_tools,
                          bsize)) {
@@ -3161,7 +3168,7 @@ static inline bool enable_palette(AV1_COMP *cpi, bool is_mode_intra,
   }
 
   if (prune_palette_testing_inter(cpi, source_variance) &&
-      best_intra_sad_norm < 10)
+      best_intra_sad_norm < sad_thresh)
     return false;
 
   if ((is_mode_intra || force_palette_test) && source_variance > 0 &&
