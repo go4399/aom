c2c8c1fdc051d2108a8c739249458374b7fb5aac
Jeremy Dorfman <jdorfman@google.com>
2025-02-27 13:27:34 -0500

    Add example sad avx2 implementation with highway
    Change-Id: I80b7653cd6edb694882c06e0118b753c3bd3d5ac

---
+++ aom_dsp/sad_hwy.cc
@@ -0,0 +1,56 @@
+#define HWY_TARGET_INCLUDE "aom_dsp/sad_hwy.cc"
+
+#include "third_party/highway/hwy/foreach_target.h"
+#include "third_party/highway/hwy/highway.h"
+
+#include "config/aom_config.h"
+
+HWY_BEFORE_NAMESPACE();
+
+#if HWY_TARGET == HWY_AVX2
+
+namespace {
+namespace HWY_NAMESPACE {
+
+namespace hn = hwy::HWY_NAMESPACE;
+
+unsigned int SumOfAbsoluteDiff64xN(const uint8_t *src_ptr, int src_stride,
+                                   const uint8_t *ref_ptr, int ref_stride,
+                                   int h) {
+  hn::CappedTag<uint8_t, 32> pixel_tag;
+  hn::CappedTag<uint64_t, 4> intermediate_sum_tag;
+  auto sum_sad = hn::Zero(intermediate_sum_tag);
+  for (int i = 0; i < h; i++) {
+    auto ref_vec_1 = hn::LoadU(pixel_tag, ref_ptr);
+    auto ref_vec_2 = hn::LoadU(pixel_tag, ref_ptr + 32);
+    auto sad_1 = hn::SumsOf8AbsDiff(ref_vec_1, hn::LoadU(pixel_tag, src_ptr));
+    auto sad_2 =
+        hn::SumsOf8AbsDiff(ref_vec_2, hn::LoadU(pixel_tag, src_ptr + 32));
+    sum_sad = hn::Add(sum_sad, hn::Add(sad_1, sad_2));
+    src_ptr += src_stride;
+    ref_ptr += ref_stride;
+  }
+
+  unsigned int res =
+      static_cast<unsigned int>(hn::ReduceSum(intermediate_sum_tag, sum_sad));
+  return res;
+}
+}  // namespace HWY_NAMESPACE
+}  // namespace
+
+HWY_AFTER_NAMESPACE();
+
+#define FSAD64_H(h)                                                           \
+  extern "C" unsigned int SumOfAbsoluteDiff64x##h##_avx2(                     \
+      const uint8_t *src_ptr, int src_stride, const uint8_t *ref_ptr,         \
+      int ref_stride) {                                                       \
+    return HWY_NAMESPACE::SumOfAbsoluteDiff64xN(src_ptr, src_stride, ref_ptr, \
+                                                ref_stride, h);               \
+  }
+
+FSAD64_H(32)
+FSAD64_H(64)
+
+#undef FSAD64_H
+
+#endif  // HWY_TARGET == HWY_AVX2
