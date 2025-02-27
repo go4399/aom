c0b0d9a3cd6c9123712a39ba1ffe039f2a8d8ec1
Jerome Jiang <jianj@google.com>
2025-02-27 13:27:34 -0500

    Add example sad avx2 implementation with highway
    Change-Id: I7dc3addc4676159cd841a6ff4180b5afa8b25808

--- build/cmake/aom_optimization.cmake
+++ build/cmake/aom_optimization.cmake
@@ -66,6 +66,12 @@ function(add_intrinsics_object_library flag opt_name target_to_update sources)
     if(${FLAG_SUPPORTED})
       set(flag "${flag} -mno-avx256-split-unaligned-store")
     endif()
+
+    unset(FLAG_SUPPORTED)
+    check_c_compiler_flag("-Wno-missing-declarations" FLAG_SUPPORTED)
+    if(${FLAG_SUPPORTED})
+      set(flag "${flag} -Wno-missing-declarations")
+    endif()
   endif()
 
   if(flag)
