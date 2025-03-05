2bf17f02dd80e44e9dbb23d6481be02629143d9f
Marco Paniconi <marpan@google.com>
2025-03-04 22:08:26 -0800

    rtc-svc: Fixes for external resize for svc
    Change-Id: Ife56858c2cb83e16a9b45fc58f5fafe691c9dee7

--- av1/encoder/aq_cyclicrefresh.c
+++ av1/encoder/aq_cyclicrefresh.c
@@ -567,13 +567,13 @@ void av1_cyclic_refresh_setup(AV1_COMP *const cpi) {
 
   // Set resolution_change flag: for svc only set it when the
   // number of spatial layers has not changed.
-  const int resolution_change =
-      cm->prev_frame &&
-      (cm->width != cm->prev_frame->width ||
-       cm->height != cm->prev_frame->height) &&
-      cpi->svc.prev_number_spatial_layers == cpi->svc.number_spatial_layers;
+  const int resolution_change = cm->prev_frame &&
+                                cpi->svc.number_spatial_layers == 1 &&
+                                (cm->width != cm->prev_frame->width ||
+                                 cm->height != cm->prev_frame->height);
 
-  if (resolution_change) cyclic_refresh_reset_resize(cpi);
+  if (resolution_change && cpi->svc.temporal_layer_id == 0)
+    cyclic_refresh_reset_resize(cpi);
   if (!cr->apply_cyclic_refresh) {
     // Don't disable and set seg_map to 0 if active_maps is enabled, unless
     // whole frame is set as inactive (since we only apply cyclic_refresh to
