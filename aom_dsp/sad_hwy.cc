5431e4914be46c1e199060937269e4a14d217689
Jerome Jiang <jianj@google.com>
2025-03-12 15:24:36 -0400

    Remove old avx2 sad
    Add more blocks to highway implementation
    
    Change-Id: Ifcabc183e781de1229ea099ce2bccc77b60d9b52

--- aom_dsp/sad_hwy.cc
+++ aom_dsp/sad_hwy.cc
@@ -53,7 +53,7 @@ HWY_MAYBE_UNUSED unsigned int SumOfAbsoluteDiff(const uint8_t *src_ptr,
 HWY_AFTER_NAMESPACE();
 
 #define FSAD(w, h, suffix)                                                   \
-  extern "C" unsigned int SumOfAbsoluteDiff##w##x##h##_##suffix(             \
+  extern "C" unsigned int aom_sad##w##x##h##_##suffix(             \
       const uint8_t *src_ptr, int src_stride, const uint8_t *ref_ptr,        \
       int ref_stride) {                                                      \
     return HWY_NAMESPACE::SumOfAbsoluteDiff<w>(src_ptr, src_stride, ref_ptr, \
@@ -61,8 +61,14 @@ HWY_AFTER_NAMESPACE();
   }
 
 #define FOR_EACH_BLOCK_SIZE(X, suffix) \
+  X(128, 128, suffix)                  \
+  X(128, 64, suffix)                   \
+  X(64, 128, suffix)                   \
+  X(64, 64, suffix)                    \
   X(64, 32, suffix)                    \
-  X(64, 64, suffix)
+  X(32, 64, suffix)                    \
+  X(32, 32, suffix)                    \
+  X(32, 16, suffix)
 
 #if HWY_TARGET == HWY_AVX2
 FOR_EACH_BLOCK_SIZE(FSAD, avx2)
@@ -71,3 +77,6 @@ FOR_EACH_BLOCK_SIZE(FSAD, avx2)
 #if HWY_TARGET == HWY_AVX3
 FOR_EACH_BLOCK_SIZE(FSAD, avx512)
 #endif  // HWY_TARGET == HWY_AVX3
+
+#undef FSAD
+#undef FOR_EACH_BLOCK_SIZE
