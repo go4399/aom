fee152e1ff47a4414471ce8f5f6d9edbbb5664d7
Jerome Jiang <jianj@google.com>
2025-03-17 18:38:21 -0400

    WIP: Add highway for sad4d
    Change-Id: I4a84c8a3d218c7c751096ac3b45551e6dca5a493

--- aom_dsp/sad_hwy.cc
+++ aom_dsp/sad_hwy.cc
@@ -59,6 +59,102 @@ HWY_MAYBE_UNUSED unsigned int SumOfAbsoluteDiff(
       hn::ReduceSum(intermediate_sum_tag, sum_sad));
 }
 
+template <size_t NumBlocks>
+struct BlockReduceTraits {};
+
+template <>
+struct BlockReduceTraits<1> {
+  template <typename D>
+  HWY_ATTR HWY_INLINE static hn::VFromD<D> ReduceSum(D int_tag,
+                                                     hn::VFromD<D> v) {
+    (void)int_tag;
+    return v;
+  }
+};
+
+template <>
+struct BlockReduceTraits<2> {
+  template <typename D>
+  HWY_ATTR HWY_INLINE static hn::VFromD<hn::BlockDFromD<D>> ReduceSum(
+      D int_tag, hn::VFromD<D> v) {
+    (void)int_tag;
+    constexpr hn::BlockDFromD<D> block_tag;
+    return hn::Add(hn::LowerHalf(block_tag, v), hn::UpperHalf(block_tag, v));
+  }
+};
+
+template <>
+struct BlockReduceTraits<4> {
+  template <typename D>
+  HWY_ATTR HWY_INLINE static hn::VFromD<hn::BlockDFromD<D>> ReduceSum(
+      D int_tag, hn::VFromD<D> v) {
+    (void)int_tag;
+    constexpr hn::Half<D> half_tag;
+    constexpr hn::BlockDFromD<D> block_tag;
+    auto vh = hn::Add(hn::LowerHalf(half_tag, v), hn::UpperHalf(half_tag, v));
+    return hn::Add(hn::LowerHalf(block_tag, vh), hn::UpperHalf(block_tag, vh));
+  }
+};
+
+template <typename D>
+HWY_ATTR HWY_INLINE hn::Vec<hn::BlockDFromD<D>> BlockReduceSum(
+    D int_tag, hn::VFromD<D> v) {
+  return BlockReduceTraits<int_tag.MaxBlocks()>::ReduceSum(int_tag, v);
+}
+
+template <int BlockWidth>
+HWY_MAYBE_UNUSED void SumOfAbsoluteDiff4D(const uint8_t *src_ptr,
+                                          int src_stride,
+                                          const uint8_t *const ref_ptr[4],
+                                          int ref_stride, int h,
+                                          uint32_t res[4]) {
+  constexpr hn::CappedTag<uint8_t, BlockWidth> pixel_tag;
+  constexpr hn::Repartition<uint64_t, decltype(pixel_tag)> intermediate_sum_tag;
+  const int vw = hn::Lanes(pixel_tag);
+  auto sum_sad_0 = hn::Zero(intermediate_sum_tag);
+  auto sum_sad_1 = hn::Zero(intermediate_sum_tag);
+  auto sum_sad_2 = hn::Zero(intermediate_sum_tag);
+  auto sum_sad_3 = hn::Zero(intermediate_sum_tag);
+  const uint8_t *ref_0, *ref_1, *ref_2, *ref_3;
+  ref_0 = ref_ptr[0];
+  ref_1 = ref_ptr[1];
+  ref_2 = ref_ptr[2];
+  ref_3 = ref_ptr[3];
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < BlockWidth; j += vw) {
+      auto src_vec = hn::LoadU(pixel_tag, &src_ptr[j]);
+      auto ref_vec_0 = hn::LoadU(pixel_tag, &ref_0[j]);
+      auto ref_vec_1 = hn::LoadU(pixel_tag, &ref_1[j]);
+      auto ref_vec_2 = hn::LoadU(pixel_tag, &ref_2[j]);
+      auto ref_vec_3 = hn::LoadU(pixel_tag, &ref_3[j]);
+      auto sad_0 = hn::SumsOf8AbsDiff(src_vec, ref_vec_0);
+      auto sad_1 = hn::SumsOf8AbsDiff(src_vec, ref_vec_1);
+      auto sad_2 = hn::SumsOf8AbsDiff(src_vec, ref_vec_2);
+      auto sad_3 = hn::SumsOf8AbsDiff(src_vec, ref_vec_3);
+      sum_sad_0 = hn::Add(sum_sad_0, sad_0);
+      sum_sad_1 = hn::Add(sum_sad_1, sad_1);
+      sum_sad_2 = hn::Add(sum_sad_2, sad_2);
+      sum_sad_3 = hn::Add(sum_sad_3, sad_3);
+    }
+    src_ptr += src_stride;
+    ref_0 += ref_stride;
+    ref_1 += ref_stride;
+    ref_2 += ref_stride;
+    ref_3 += ref_stride;
+  }
+  constexpr hn::Repartition<uint32_t, decltype(pixel_tag)> uint32_tag;
+  auto r02 = hn::InterleaveEven(uint32_tag, hn::BitCast(uint32_tag, sum_sad_0),
+                                hn::BitCast(uint32_tag, sum_sad_2));
+  auto r13 = hn::InterleaveEven(uint32_tag, hn::BitCast(uint32_tag, sum_sad_1),
+                                hn::BitCast(uint32_tag, sum_sad_3));
+  auto r0123 = hn::Add(hn::InterleaveLower(uint32_tag, r02, r13),
+                       hn::InterleaveUpper(uint32_tag, r02, r13));
+
+  auto block_sum = BlockReduceSum(uint32_tag, r0123);
+  constexpr hn::CappedTag<uint32_t, 4> block_sum_tag;
+  hn::StoreU(block_sum, block_sum_tag, res);
+}
+
 }  // namespace HWY_NAMESPACE
 }  // namespace
 
@@ -68,6 +164,14 @@ HWY_MAYBE_UNUSED unsigned int SumOfAbsoluteDiff(
       int ref_stride) {                                                      \
     return HWY_NAMESPACE::SumOfAbsoluteDiff<w>(src_ptr, src_stride, ref_ptr, \
                                                ref_stride, h);               \
+  }
+
+#define FSAD4D(w, h, suffix)                                                   \
+  extern "C" HWY_ATTR void aom_sad##w##x##h##x4d_##suffix(                     \
+      const uint8_t *src_ptr, int src_stride, const uint8_t *const ref_ptr[4], \
+      int ref_stride, uint32_t res[4]) {                                       \
+    HWY_NAMESPACE::SumOfAbsoluteDiff4D<w>(src_ptr, src_stride, ref_ptr,        \
+                                          ref_stride, h, res);                 \
   }
 
 #define FSADSKIP(w, h, suffix)                                               \
@@ -97,6 +201,7 @@ HWY_MAYBE_UNUSED unsigned int SumOfAbsoluteDiff(
 FOR_EACH_BLOCK_SIZE(FSAD, avx2)
 FOR_EACH_BLOCK_SIZE(FSADSKIP, avx2)
 FOR_EACH_BLOCK_SIZE(FSADAVG, avx2)
+FOR_EACH_BLOCK_SIZE(FSAD4D, avx2)
 #endif  // HWY_TARGET == HWY_AVX2
 
 #if HWY_TARGET == HWY_AVX3
