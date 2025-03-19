3707c19d1f1872099c31991b44162ac1ed844680
Jeremy Dorfman <jdorfman@google.com>
2025-03-19 13:18:11 -0400

    yv12config: Pad strides for AVX512
    Change-Id: Ic67149e49942b6180ae36ac77f1bf24ec9e2db2e

--- aom_scale/yv12config.h
+++ aom_scale/yv12config.h
@@ -213,7 +213,7 @@ int aom_copy_metadata_to_frame_buffer(YV12_BUFFER_CONFIG *ybf,
  * \param[in]    border              Border in pixels
  */
 static inline int aom_calc_y_stride(int aligned_width, int border) {
-  return ((aligned_width + 2 * border) + 31) & ~31;
+  return ((aligned_width + 2 * border) + 63) & ~63;
 }
 
 #ifdef __cplusplus
