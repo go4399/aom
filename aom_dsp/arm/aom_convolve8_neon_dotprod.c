3b95764d369cf342add1d95cb63035ec46966ae1
Jerome Jiang <jianj@google.com>
2025-03-28 17:47:21 -0400

    highway: add aom_convolve8_horiz
    Move get_filter_taps_convolve8 to aom_dsp/aom_filter.h to be shared
    by x86 and arm.
    
    Change-Id: I7d71206418d4fc5497c03470569ab43b3e2f552b

--- aom_dsp/arm/aom_convolve8_neon_dotprod.c
+++ aom_dsp/arm/aom_convolve8_neon_dotprod.c
@@ -21,7 +21,6 @@
 #include "aom_dsp/aom_dsp_common.h"
 #include "aom_dsp/aom_filter.h"
 #include "aom_dsp/arm/aom_convolve8_neon.h"
-#include "aom_dsp/arm/aom_filter.h"
 #include "aom_dsp/arm/mem_neon.h"
 #include "aom_dsp/arm/transpose_neon.h"
 #include "aom_ports/mem.h"
