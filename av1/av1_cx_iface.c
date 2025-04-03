a9b03a2001ae714a2a823dec266ed995e183780a
Marco Paniconi <marpan@google.com>
2025-04-03 11:39:05 -0700

    Fix to update seq_params for number of layers change
    If the number of layers change the sequence params
    (i.e., operating_points_cnt_minus_1 and operating_point_idc[])
    need to be updated. The change in layers is done via the
    set_svc_params control, so set seq_params_locked = 0 there
    if the number of spatial or temporal layers have changed.
    
    This fixes the assert in the bug below.
    Added unittest that changes the number of temporal layers
    from 1->3->1, which also triggers the assert.
    
    Bug: b:407813259
    Change-Id: I99416f0cab1cf095a62d533ddc21a7e71dcb831a

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -3509,6 +3509,8 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
       }
 
       ppi->seq_params_locked = 1;
+      cpi->svc.prev_number_temporal_layers = cpi->svc.number_temporal_layers;
+      cpi->svc.prev_number_spatial_layers = cpi->svc.number_spatial_layers;
       av1_post_encode_updates(cpi, &cpi_data);
 
 #if CONFIG_ENTROPY_STATS
@@ -3883,6 +3885,16 @@ static aom_codec_err_t ctrl_set_svc_params(aom_codec_alg_priv_t *ctx,
   ppi->number_temporal_layers = params->number_temporal_layers;
   cpi->svc.number_spatial_layers = params->number_spatial_layers;
   cpi->svc.number_temporal_layers = params->number_temporal_layers;
+  // Sequence parameters (operating_points_cnt_minus_1, operating_point_idc[])
+  // need to be updated if the number or layers have changed, so set the
+  // seq_params_locked to 0 under this condition. Another option is leave
+  // seq_params_locked as is and just update the two fields here.
+  if (ppi->seq_params_locked &&
+      (cpi->svc.number_temporal_layers !=
+           cpi->svc.prev_number_temporal_layers ||
+       cpi->svc.number_spatial_layers != cpi->svc.prev_number_spatial_layers)) {
+    ppi->seq_params_locked = 0;
+  }
   if (ppi->number_spatial_layers > 1 || ppi->number_temporal_layers > 1) {
     unsigned int sl, tl;
     ctx->ppi->use_svc = 1;
