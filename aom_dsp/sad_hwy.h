7de8d1582a453eb5c6a0ef35ecb0cee5a5478d50
Jerome Jiang <jianj@google.com>
2025-04-04 10:42:45 -0400

    Fix -Wmissing-prototypes / -Wmissing-declarations
    Disable benchmark by default on all platforms
    
    Change-Id: I7460ed387a9fc8fc73d0bb2a2987e39d9dc81ab9

--- aom_dsp/sad_hwy.h
+++ aom_dsp/sad_hwy.h
@@ -117,7 +117,10 @@ HWY_MAYBE_UNUSED void SumOfAbsoluteDiffND(const uint8_t *src_ptr,
 }  // namespace
 
 #define FSAD(w, h, suffix)                                                   \
-  extern "C" HWY_ATTR unsigned int aom_sad##w##x##h##_##suffix(              \
+  extern "C" unsigned int aom_sad##w##x##h##_##suffix(                       \
+      const uint8_t *src_ptr, int src_stride, const uint8_t *ref_ptr,        \
+      int ref_stride);                                                       \
+  HWY_ATTR unsigned int aom_sad##w##x##h##_##suffix(                         \
       const uint8_t *src_ptr, int src_stride, const uint8_t *ref_ptr,        \
       int ref_stride) {                                                      \
     return HWY_NAMESPACE::SumOfAbsoluteDiff<w>(src_ptr, src_stride, ref_ptr, \
@@ -125,7 +128,10 @@ HWY_MAYBE_UNUSED void SumOfAbsoluteDiffND(const uint8_t *src_ptr,
   }
 
 #define FSAD4D(w, h, suffix)                                                   \
-  extern "C" HWY_ATTR void aom_sad##w##x##h##x4d_##suffix(                     \
+  extern "C" void aom_sad##w##x##h##x4d_##suffix(                              \
+      const uint8_t *src_ptr, int src_stride, const uint8_t *const ref_ptr[4], \
+      int ref_stride, uint32_t res[4]);                                        \
+  HWY_ATTR void aom_sad##w##x##h##x4d_##suffix(                                \
       const uint8_t *src_ptr, int src_stride, const uint8_t *const ref_ptr[4], \
       int ref_stride, uint32_t res[4]) {                                       \
     HWY_NAMESPACE::SumOfAbsoluteDiffND<w, 4>(src_ptr, src_stride, ref_ptr,     \
@@ -133,7 +139,10 @@ HWY_MAYBE_UNUSED void SumOfAbsoluteDiffND(const uint8_t *src_ptr,
   }
 
 #define FSAD4DSKIP(w, h, suffix)                                               \
-  extern "C" HWY_ATTR void aom_sad_skip_##w##x##h##x4d_##suffix(               \
+  extern "C" void aom_sad_skip_##w##x##h##x4d_##suffix(                        \
+      const uint8_t *src_ptr, int src_stride, const uint8_t *const ref_ptr[4], \
+      int ref_stride, uint32_t res[4]);                                        \
+  HWY_ATTR void aom_sad_skip_##w##x##h##x4d_##suffix(                          \
       const uint8_t *src_ptr, int src_stride, const uint8_t *const ref_ptr[4], \
       int ref_stride, uint32_t res[4]) {                                       \
     HWY_NAMESPACE::SumOfAbsoluteDiffND<w, 4>(src_ptr, 2 * src_stride, ref_ptr, \
@@ -145,7 +154,10 @@ HWY_MAYBE_UNUSED void SumOfAbsoluteDiffND(const uint8_t *src_ptr,
   }
 
 #define FSAD3D(w, h, suffix)                                                   \
-  extern "C" HWY_ATTR void aom_sad##w##x##h##x3d_##suffix(                     \
+  extern "C" void aom_sad##w##x##h##x3d_##suffix(                              \
+      const uint8_t *src_ptr, int src_stride, const uint8_t *const ref_ptr[4], \
+      int ref_stride, uint32_t res[4]);                                        \
+  HWY_ATTR void aom_sad##w##x##h##x3d_##suffix(                                \
       const uint8_t *src_ptr, int src_stride, const uint8_t *const ref_ptr[4], \
       int ref_stride, uint32_t res[4]) {                                       \
     HWY_NAMESPACE::SumOfAbsoluteDiffND<w, 3>(src_ptr, src_stride, ref_ptr,     \
@@ -153,7 +165,10 @@ HWY_MAYBE_UNUSED void SumOfAbsoluteDiffND(const uint8_t *src_ptr,
   }
 
 #define FSADSKIP(w, h, suffix)                                               \
-  extern "C" HWY_ATTR unsigned int aom_sad_skip_##w##x##h##_##suffix(        \
+  extern "C" unsigned int aom_sad_skip_##w##x##h##_##suffix(                 \
+      const uint8_t *src_ptr, int src_stride, const uint8_t *ref_ptr,        \
+      int ref_stride);                                                       \
+  HWY_ATTR unsigned int aom_sad_skip_##w##x##h##_##suffix(                   \
       const uint8_t *src_ptr, int src_stride, const uint8_t *ref_ptr,        \
       int ref_stride) {                                                      \
     return 2 * HWY_NAMESPACE::SumOfAbsoluteDiff<w>(                          \
@@ -161,7 +176,10 @@ HWY_MAYBE_UNUSED void SumOfAbsoluteDiffND(const uint8_t *src_ptr,
   }
 
 #define FSADAVG(w, h, suffix)                                                \
-  extern "C" HWY_ATTR unsigned int aom_sad##w##x##h##_avg_##suffix(          \
+  extern "C" unsigned int aom_sad##w##x##h##_avg_##suffix(                   \
+      const uint8_t *src_ptr, int src_stride, const uint8_t *ref_ptr,        \
+      int ref_stride, const uint8_t *second_pred);                           \
+  HWY_ATTR unsigned int aom_sad##w##x##h##_avg_##suffix(                     \
       const uint8_t *src_ptr, int src_stride, const uint8_t *ref_ptr,        \
       int ref_stride, const uint8_t *second_pred) {                          \
     return HWY_NAMESPACE::SumOfAbsoluteDiff<w>(src_ptr, src_stride, ref_ptr, \
