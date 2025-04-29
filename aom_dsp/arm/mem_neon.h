d5158ceeee6358bea61b46bc104dd2f8106f2ddc
James Zern <jzern@google.com>
2025-04-29 13:39:12 -0700

    mem_neon.h: update GCC_32_BIT check
    gcc 14 includes all of the intrinsics in this block. Avoid redefining
    them to prevent a build failure with certain versions of gcc 14 that
    used incorrect pointer signs for vld1q_s8_x4, vld1q_s16_x4, vld1q_s8_x3
    and vld1q_s16_x3.
    
    Fixes:
    aom_dsp/arm/mem_neon.h:63:27: error: conflicting types for
      'vld1q_s16_x4'; have 'int16x8x4_t(const int16_t *)' {aka
      'int16x8x4_t(const short int *)'}
       63 | static inline int16x8x4_t vld1q_s16_x4(const int16_t *ptr) {
    
    Bug: aomedia:397711740
    Fixed: aomedia:397711740
    Change-Id: I33737e2e9d63bed2ea075a5fc1779549a7f753f1

--- aom_dsp/arm/mem_neon.h
+++ aom_dsp/arm/mem_neon.h
@@ -27,11 +27,12 @@
 #define DEFICIENT_CLANG_32_BIT  // This includes clang-cl.
 #endif
 
-#if defined(__GNUC__) && !defined(__clang__) && defined(ARM_32_BIT)
-#define GCC_32_BIT
+#if defined(__GNUC__) && !defined(__clang__) && defined(ARM_32_BIT) && \
+    __GNUC__ < 14
+#define DEFICIENT_GCC_32_BIT
 #endif
 
-#if defined(DEFICIENT_CLANG_32_BIT) || defined(GCC_32_BIT)
+#if defined(DEFICIENT_CLANG_32_BIT) || defined(DEFICIENT_GCC_32_BIT)
 
 static inline uint8x16x3_t vld1q_u8_x3(const uint8_t *ptr) {
   uint8x16x3_t res = { { vld1q_u8(ptr + 0 * 16), vld1q_u8(ptr + 1 * 16),
