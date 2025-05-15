c4229df898bf7a36be5426d8c26ff3b49db90427
Jingning Han <jingning@google.com>
2025-05-15 10:39:41 -0700

    Add high bd support to variance calculation
    BUG=b/417883707
    
    Change-Id: I0c5654bd0a5a8c8baf81f857c013f2432e17b13d

--- av1/encoder/temporal_filter.c
+++ av1/encoder/temporal_filter.c
@@ -203,14 +203,24 @@ static void tf_motion_search(AV1_COMP *cpi, MACROBLOCK *mb,
   int distortion;
   int cost_list[5];
 
-  DECLARE_ALIGNED(16, uint8_t, dclevel[MAX_SB_SQUARE]);
-  memset(dclevel, 128, sizeof(dclevel));
+  const int is_high_bitdepth = is_cur_buf_hbd(mbd);
+
+  DECLARE_ALIGNED(16, uint16_t, dclevel16[MAX_SB_SQUARE]);
+  DECLARE_ALIGNED(16, uint8_t, dclevel8[MAX_SB_SQUARE]);
+
+  if (is_high_bitdepth)
+    memset(dclevel16, 0, sizeof(dclevel16));
+  else
+    memset(dclevel8, 0, sizeof(dclevel8));
+
   int dclevel_stride = block_size_wide[block_size];
   int64_t src_var = INT32_MAX;
 
   if (cpi->oxcf.algo_cfg.sharpness)
-    src_var = cpi->ppi->fn_ptr[block_size].vf(mb->plane[0].src.buf, y_stride,
-                                              dclevel, dclevel_stride, &sse);
+    src_var = cpi->ppi->fn_ptr[block_size].vf(
+        mb->plane[0].src.buf, y_stride,
+        is_high_bitdepth ? CONVERT_TO_BYTEPTR(dclevel16) : dclevel8,
+        dclevel_stride, &sse);
 
   // Do motion search.
   int_mv best_mv;  // Searched motion vector.
