c51b84a1e3e02c6ff7653651b3fbdb263db7b237
Jerry Tsai <jerytsai@andestech.com>
2025-06-20 17:03:54 +0800

    riscv64: Add highbd convolve optimization
    Change-Id: Icfd542213522bc39b324b2b58cf8d8eeb930653c

---
+++ av1/common/riscv/highbd_convolve_rvv.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025, Alliance for Open Media. All rights reserved.
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#ifndef AOM_AV1_COMMON_RISCV_HIGHBD_CONVOLVE_RVV_H_
+#define AOM_AV1_COMMON_RISCV_HIGHBD_CONVOLVE_RVV_H_
+
+#include "aom_dsp/riscv/mem_rvv.h"
+
+#include "av1/common/convolve.h"
+#include "av1/common/riscv/convolve_rvv.h"
+
+#endif  // AOM_AV1_COMMON_RISCV_HIGHBD_CONVOLVE_RVV_H_
