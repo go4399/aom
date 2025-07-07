b7ebaa6a5412783d2d17335c58fce53193c9d686
Julio Barba <juliobbv@gmail.com>
2025-07-07 17:31:31 -0400

    intraBC: add speed features
    Add the following speed features to control intraBC's speed/
    efficiency tradeoffs, and enable them in all intra mode as follows:
    
    | Speed feature                      | Speed |
    |------------------------------------|-------|
    | prune_intrabc_candidate_block_hash | >= 1  |
    | hash_max_8x8_intrabc_blocks        | >= 4  |
    | intrabc_search_level               | >= 6  |
    
    Screenshot example (QP 40):
    
    | Speed 3          | Size (bytes) | Time (ms) | PSNR   |
    |------------------|--------------|-----------|--------|
    | intraBC (before) | 82434        | 47826     | 43.410 |
    | intraBC (after)  | 82673        | 9933      | 43.598 |
    | No intraBC       | 175255       | 5599      | 42.206 |
    
    | Speed 6          | Size (bytes) | Time (ms) | PSNR   |
    |------------------|--------------|-----------|--------|
    | intraBC (before) | 94378        | 6282      | 43.468 |
    | intraBC (after)  | 98923        | 1284      | 43.628 |
    | No intraBC       | 192461       | 669       | 39.909 |
    
    Bug: aomedia:421196988
    Change-Id: I0f8540b77bf87d0b4a1612b134012e5462d8837e

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -2088,7 +2088,7 @@ static inline void encode_frame_internal(AV1_COMP *cpi) {
   }
 
   features->allow_intrabc &= (oxcf->kf_cfg.enable_intrabc);
-
+  // features->allow_intrabc = 0;
   if (features->allow_warped_motion &&
       cpi->sf.inter_sf.prune_warped_prob_thresh > 0) {
     const FRAME_UPDATE_TYPE update_type =
@@ -2146,8 +2146,12 @@ static inline void encode_frame_internal(AV1_COMP *cpi) {
                                       block_hash_values[0], is_block_same[0]);
     // Hash data generated for screen contents is used for intraBC ME
     const int min_alloc_size = block_size_wide[mi_params->mi_alloc_bsize];
-    const int max_sb_size =
-        (1 << (cm->seq_params->mib_size_log2 + MI_SIZE_LOG2));
+    int max_sb_size = (1 << (cm->seq_params->mib_size_log2 + MI_SIZE_LOG2));
+
+    if (cpi->sf.mv_sf.hash_max_8x8_intrabc_blocks) {
+      max_sb_size = AOMMIN(8, max_sb_size);
+    }
+
     int src_idx = 0;
     for (int size = 4; size <= max_sb_size; size *= 2, src_idx = !src_idx) {
       const int dst_idx = !src_idx;
@@ -2686,7 +2690,6 @@ void av1_encode_frame(AV1_COMP *cpi) {
 
     rdc->compound_ref_used_flag = 0;
     rdc->skip_mode_used_flag = 0;
-
     encode_frame_internal(cpi);
 
     if (current_frame->reference_mode == REFERENCE_MODE_SELECT) {
