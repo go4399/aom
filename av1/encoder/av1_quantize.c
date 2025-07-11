97cd1fcea794d026473a30fd251b2a37ce2180eb
Jingning Han <jingning@google.com>
2025-07-11 10:29:31 -0700

    Disable chroma delta q in lossless mode
    BUG=b/430090587
    
    Change-Id: I6610ef37aa1b9fc866fdbaec1cc94b1a53570b06

--- av1/encoder/av1_quantize.c
+++ av1/encoder/av1_quantize.c
@@ -883,7 +883,8 @@ void av1_set_quantizer(AV1_COMMON *const cm, int min_qmlevel, int max_qmlevel,
   quant_params->base_qindex = AOMMAX(cm->delta_q_info.delta_q_present_flag, q);
   quant_params->y_dc_delta_q = 0;
 
-  if (enable_chroma_deltaq) {
+  // Disable deltaq in lossless mode.
+  if (enable_chroma_deltaq && q) {
     if (is_allintra &&
         (tuning == AOM_TUNE_IQ || tuning == AOM_TUNE_SSIMULACRA2)) {
       int chroma_dc_delta_q = 0;
