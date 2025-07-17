bf294af212bd03ded8614e4a7e2f512c1a95199c
Julio Barba <juliobbv@gmail.com>
2025-07-17 17:30:27 -0400

    Introduce adaptive sharpness
    This new setting modulates sharpness based on frame QP, which helps
    mitigate blocking artifacts in the low to medium quality range.
    
    This can be enabled by parameter `--enable-adaptive-sharpness=1`, or
    by using codec control AV1E_SET_ENABLE_ADAPTIVE_SHARPNESS.
    
    Enabling adaptive sharpness comes with a very small (but measurable)
    impact on SSIMULACRA2 scores (Daala's subset1), so only enable by
    default for AOM_TUNE_IQ.
    
    Bug: aomedia:401234702
    Change-Id: I14f5bf0aab4e56f23704f28c502851bd1a95424c

--- av1/arg_defs.h
+++ av1/arg_defs.h
@@ -238,6 +238,7 @@ typedef struct av1_codec_arg_definitions {
   arg_def_t sb_qp_sweep;
   arg_def_t enable_low_complexity_decode;
   arg_def_t screen_detection_mode;
+  arg_def_t enable_adaptive_sharpness;
 #endif  // CONFIG_AV1_ENCODER
 } av1_codec_arg_definitions_t;
 
