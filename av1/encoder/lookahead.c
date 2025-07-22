66e526c6331afffbc35070114b949d9121304247
Jerome Jiang <jianj@google.com>
2025-07-22 16:00:59 -0400

    Cosmetic: Fix clang-tidy warnings
    Change-Id: Ic10fd080b785ba4e2aae710eb5818ac6ae62c23c

--- av1/encoder/lookahead.c
+++ av1/encoder/lookahead.c
@@ -45,7 +45,7 @@ void av1_lookahead_destroy(struct lookahead_ctx *ctx) {
 struct lookahead_ctx *av1_lookahead_init(int width, int height,
                                          int subsampling_x, int subsampling_y,
                                          int use_highbitdepth, int depth,
-                                         const int border_in_pixels,
+                                         int border_in_pixels,
                                          int byte_alignment,
                                          int num_lap_buffers, bool is_all_intra,
                                          bool alloc_pyramid) {
