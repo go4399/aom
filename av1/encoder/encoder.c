e8762d5cf019553fd7b23a6e49cd0b7420fab95d
Erik Språng <sprang@google.com>
2025-09-08 16:10:09 +0200

    feat: Add AOM_EFLAG_FREEZE_INTERNAL_STATE
    Add a flag to encode a frame without updating encoder state.
    This is useful for speculative encoding.
    
    The flag ensures the current frame is encoded normally, PSNR is
    generated if requested, but internal state like reference buffers,
    CDF tables, and rate control is not modified.
    
    Adds tests for single layer and SVC encoding.
    
    Change-Id: I34e9e3d0beb355cff2f9b3419820773e5d8c550d

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -1867,7 +1867,7 @@ void av1_remove_compressor(AV1_COMP *cpi) {
 #endif
 }
 
-static void generate_psnr_packet(AV1_COMP *cpi) {
+void generate_psnr_packet(AV1_COMP *cpi) {
   struct aom_codec_cx_pkt pkt;
   int i;
   PSNR_STATS psnr;
@@ -5073,6 +5073,20 @@ void av1_post_encode_updates(AV1_COMP *const cpi,
                              const AV1_COMP_DATA *const cpi_data) {
   AV1_PRIMARY *const ppi = cpi->ppi;
   AV1_COMMON *const cm = &cpi->common;
+
+  if (ppi->b_freeze_internal_state) {
+    // Should not update encoder state, just necessary work to get the
+    // expected output and then return early.
+
+    // Note *size = 0 indicates a dropped frame for which psnr is not calculated
+    if (ppi->b_calculate_psnr && cpi_data->frame_size > 0) {
+      if (cm->show_existing_frame ||
+          (!is_stat_generation_stage(cpi) && cm->show_frame)) {
+        generate_psnr_packet(cpi);
+      }
+    }
+    return;
+  }
 
   update_gm_stats(cpi);
 
