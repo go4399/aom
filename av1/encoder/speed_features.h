1ca42caa3ff8f5230cb81a59c1554a1fd5e682d6
Marco Paniconi <marpan@google.com>
2025-09-24 14:37:08 -0700

    rtc: Update conditions to active_worst_quality for key
    CBR RC: Add more/stronger conditions to allowing lower
    active_worst_quality for keyframes. And modify the logic
    for the computation of frame spatial variance to put it
    around the flag sf->rc_compute_spatial_var_sc.
    
    Change-Id: Ic131fcaca813d253074204ffdf6c026e805149cf

--- av1/encoder/speed_features.h
+++ av1/encoder/speed_features.h
@@ -1727,7 +1727,7 @@ typedef struct REAL_TIME_SPEED_FEATURES {
   // For keyframes in rtc: adjust the rc_bits_per_mb, to reduce overshoot.
   int rc_adjust_keyframe;
 
-  // On scene change: compute spatial variance.
+  // On scene change or keyframe: compute spatial variance.
   int rc_compute_spatial_var_sc;
 
   // For nonrd mode: Prefer larger partition blks in variance based partitioning
