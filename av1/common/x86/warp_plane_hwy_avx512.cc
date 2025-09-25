go4399/aom

commit 353c63faba3c3af36430ff168cf5e149b760d42d

warp: Highway implementation for SSE4/AVX2/AVX512
This ends up faster in all cases by around 5%, and the AVX512
implementation ends up another 5% faster than AVX2.

Change-Id: I50351310b3cfe26b6182e1cd96c0b18f83ec09eb

av1/common/x86/warp_plane_hwy_avx512.cc
@@ -1,0 +1,17 @@
+/*
+ * Copyright (c) 2025, Alliance for Open Media. All rights reserved.
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#define HWY_BASELINE_TARGETS HWY_AVX3_DL
+#define HWY_BROKEN_32BIT 0
+
+#include "av1/common/warp_plane_hwy.h"
+
+MAKE_WARP_AFFINE(avx512)

av1/av1.cmake
@@ -94,6 +94,7 @@ list(APPEND AOM_AV1_COMMON_SOURCES
 
 if(CONFIG_HIGHWAY)
   list(APPEND AOM_AV1_COMMON_SOURCES "${AOM_ROOT}/av1/common/selfguided_hwy.h")
+  list(APPEND AOM_AV1_COMMON_SOURCES "${AOM_ROOT}/av1/common/warp_plane_hwy.h")
 endif()
 
 list(APPEND AOM_AV1_DECODER_SOURCES
@@ -321,8 +322,13 @@ list(APPEND AOM_AV1_COMMON_INTRIN_AVX2
             "${AOM_ROOT}/av1/common/x86/wiener_convolve_avx2.c")
 
 if(CONFIG_HIGHWAY)
+  list(APPEND AOM_AV1_COMMON_INTRIN_SSE4_1
+              "${AOM_ROOT}/av1/common/x86/warp_plane_hwy_sse4.cc")
+  list(APPEND AOM_AV1_COMMON_INTRIN_AVX2
+              "${AOM_ROOT}/av1/common/x86/warp_plane_hwy_avx2.cc")
   list(APPEND AOM_AV1_COMMON_INTRIN_AVX512
-              "${AOM_ROOT}/av1/common/x86/selfguided_hwy_avx512.cc")
+              "${AOM_ROOT}/av1/common/x86/selfguided_hwy_avx512.cc"
+              "${AOM_ROOT}/av1/common/x86/warp_plane_hwy_avx512.cc")
 endif()
 
 list(APPEND AOM_AV1_ENCODER_ASM_SSE2 "${AOM_ROOT}/av1/encoder/x86/dct_sse2.asm"
@@ -591,7 +597,8 @@ if(CONFIG_REALTIME_ONLY)
                      "${AOM_ROOT}/av1/common/restoration.h"
                      "${AOM_ROOT}/av1/common/selfguided_hwy.h"
                      "${AOM_ROOT}/av1/common/warped_motion.c"
-                     "${AOM_ROOT}/av1/common/warped_motion.h")
+                     "${AOM_ROOT}/av1/common/warped_motion.h"
+                     "${AOM_ROOT}/av1/common/warp_plane_hwy.h")
 
     list(REMOVE_ITEM AOM_AV1_COMMON_INTRIN_SSE2
                      "${AOM_ROOT}/av1/common/x86/cfl_sse2.c"
@@ -601,7 +608,8 @@ if(CONFIG_REALTIME_ONLY)
     list(REMOVE_ITEM AOM_AV1_COMMON_INTRIN_SSE4_1
                      "${AOM_ROOT}/av1/common/x86/highbd_warp_plane_sse4.c"
                      "${AOM_ROOT}/av1/common/x86/selfguided_sse4.c"
-                     "${AOM_ROOT}/av1/common/x86/warp_plane_sse4.c")
+                     "${AOM_ROOT}/av1/common/x86/warp_plane_sse4.c"
+                     "${AOM_ROOT}/av1/common/x86/warp_plane_hwy_sse4.cc")
 
     list(
       REMOVE_ITEM AOM_AV1_COMMON_INTRIN_SSSE3
@@ -614,10 +622,12 @@ if(CONFIG_REALTIME_ONLY)
                      "${AOM_ROOT}/av1/common/x86/highbd_wiener_convolve_avx2.c"
                      "${AOM_ROOT}/av1/common/x86/selfguided_avx2.c"
                      "${AOM_ROOT}/av1/common/x86/warp_plane_avx2.c"
+                     "${AOM_ROOT}/av1/common/x86/warp_plane_hwy_avx2.cc"
                      "${AOM_ROOT}/av1/common/x86/wiener_convolve_avx2.c")
 
     list(REMOVE_ITEM AOM_AV1_COMMON_INTRIN_AVX512
-                     "${AOM_ROOT}/av1/common/x86/selfguided_hwy_avx512.cc")
+                     "${AOM_ROOT}/av1/common/x86/selfguided_hwy_avx512.cc"
+                     "${AOM_ROOT}/av1/common/x86/warp_plane_hwy_avx512.cc")
 
     list(REMOVE_ITEM AOM_AV1_COMMON_INTRIN_NEON
                      "${AOM_ROOT}/av1/common/arm/cfl_neon.c"

av1/common/av1_rtcd_defs.pl
@@ -557,6 +557,9 @@ specialize qw/av1_resize_horz_dir sse2 avx2/;
 if ((aom_config("CONFIG_REALTIME_ONLY") ne "yes") || (aom_config("CONFIG_AV1_DECODER") eq "yes")) {
   add_proto qw/void av1_warp_affine/, "const int32_t *mat, const uint8_t *ref, int width, int height, int stride, uint8_t *pred, int p_col, int p_row, int p_width, int p_height, int p_stride, int subsampling_x, int subsampling_y, ConvolveParams *conv_params, int16_t alpha, int16_t beta, int16_t gamma, int16_t delta";
   specialize qw/av1_warp_affine sse4_1 avx2 neon neon_i8mm sve/;
+  if (aom_config("CONFIG_HIGHWAY") eq "yes") {
+    specialize qw/av1_warp_affine avx512/;
+  }
 }
 
 # LOOP_RESTORATION functions

av1/common/x86/warp_plane_avx2.c
@@ -14,6 +14,8 @@
 #include "av1/common/warped_motion.h"
 #include "aom_dsp/x86/synonyms.h"
 
+#if !CONFIG_HIGHWAY
+
 DECLARE_ALIGNED(32, static const uint8_t, shuffle_alpha0_mask01_avx2[32]) = {
   0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
   0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1
@@ -1208,3 +1210,5 @@ void av1_warp_affine_avx2(const int32_t *mat, const uint8_t *ref, int width,
     }
   }
 }
+
+#endif  // !CONFIG_HIGHWAY

av1/common/x86/warp_plane_hwy_avx2.cc
@@ -1,0 +1,17 @@
+/*
+ * Copyright (c) 2025, Alliance for Open Media. All rights reserved.
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#define HWY_BASELINE_TARGETS HWY_AVX2
+#define HWY_BROKEN_32BIT 0
+
+#include "av1/common/warp_plane_hwy.h"
+
+MAKE_WARP_AFFINE(avx2)

av1/common/x86/warp_plane_hwy_sse4.cc
@@ -1,0 +1,17 @@
+/*
+ * Copyright (c) 2025, Alliance for Open Media. All rights reserved.
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#define HWY_BASELINE_TARGETS HWY_SSE4
+#define HWY_BROKEN_32BIT 0
+
+#include "av1/common/warp_plane_hwy.h"
+
+MAKE_WARP_AFFINE(sse4_1)

av1/common/x86/warp_plane_sse4.c
@@ -137,6 +137,8 @@ DECLARE_ALIGNED(8, const int8_t,
 };
 /* clang-format on */
 
+#if !CONFIG_HIGHWAY
+
 // Shuffle masks: we want to convert a sequence of bytes 0, 1, 2, ..., 15
 // in an SSE register into two sequences:
 // 0, 2, 2, 4, ..., 12, 12, 14, <don't care>
@@ -906,3 +908,5 @@ void av1_warp_affine_sse4_1(const int32_t *mat, const uint8_t *ref, int width,
     }
   }
 }
+
+#endif  // !CONFIG_HIGHWAY

commit 63eb57642d1fdb8b5aa2fcd2b5520b48b2b7d028

Temporarily disable hwy AVX512 for warp
Known issues is in AVX512. Let's force it to use AVX2 until the fix.

The issue is introduced in
353c63faba3c3af36430ff168cf5e149b760d42d

Change-Id: Idaa10e017d197853b4ab231656f15c40e58265c0

av1/common/x86/warp_plane_hwy_avx512.cc
@@ -9,7 +9,11 @@
  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
 
-#define HWY_BASELINE_TARGETS HWY_AVX3_DL
+// TODO(any): temporarily disable AVX512 for warp due to known test failure.
+// https://b.corp.google.com/issues/447385711
+// Renable it after the test failure is resolved.
+#define HWY_BASELINE_TARGETS HWY_AVX2
+//#define HWY_BASELINE_TARGETS HWY_AVX3_DL
 #define HWY_BROKEN_32BIT 0
 
 #include "av1/common/warp_plane_hwy.h"