578c05687a971fa3f979790dd54b80eacbaf05ae
Jingning Han <jingning@google.com>
2025-10-03 23:46:30 -0700

    WIP test
    Change-Id: If74c6b154a33debabc3bd8d265acbffd0129076f

--- av1/encoder/encodemb.c
+++ av1/encoder/encodemb.c
@@ -865,6 +865,7 @@ void av1_encode_intra_block_plane(const struct AV1_COMP *cpi, MACROBLOCK *x,
   if (enable_optimize_b) {
     av1_get_entropy_contexts(plane_bsize, pd, ta, tl);
   }
+
   av1_foreach_transformed_block_in_plane(
       xd, plane_bsize, plane, encode_block_intra_and_set_context, &arg);
 }
