e709d5e6af60145537918c1991c1f9d2048d8ec0
Jerome Jiang <jianj@google.com>
2025-10-08 14:37:42 -0400

    RC: Add codec control and impl for init/create
    Bug: aomedia:450252793
    
    Change-Id: Ida28381348130ff0cf7a6c84781099baeb2817e7

--- build/cmake/aom_install.cmake
+++ build/cmake/aom_install.cmake
@@ -20,6 +20,7 @@ endif()
 if(CONFIG_AV1_ENCODER)
   list(APPEND AOM_INSTALL_INCS "${AOM_ROOT}/aom/aomcx.h"
               "${AOM_ROOT}/aom/aom_encoder.h"
+              "${AOM_ROOT}/aom/aom_ext_ratectrl.h"
               "${AOM_ROOT}/aom/aom_external_partition.h")
 endif()
 
