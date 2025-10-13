b7139c33cc1e85290d5b9226bb251ef8539007f2
Marco Paniconi <marpan@google.com>
2025-10-12 21:59:29 -0700

    Move the av1_alloc_layer_context up in set_svc_params
    The av1_alloc_layer_context() should be called once the
    number of layers are set and use_svc is set to 1.
    Otherwise if set_svc_params fails due to invalid params
    before av1_alloc_layer_context is called, av1_free_svc_cyclic_refresh()
    can fail, as in the issue below.
    
    Bug: b:449341177
    Change-Id: Id5b3ad3b2ec36a77e5415633a5ebfb2d970ec615

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -4014,6 +4014,7 @@ static aom_codec_err_t ctrl_set_svc_params(aom_codec_alg_priv_t *ctx,
     ctx->ppi->use_svc = 1;
     const int num_layers =
         ppi->number_spatial_layers * ppi->number_temporal_layers;
+    if (!av1_alloc_layer_context(cpi, num_layers)) return AOM_CODEC_MEM_ERROR;
     for (int layer = 0; layer < num_layers; ++layer) {
       if (params->max_quantizers[layer] > 63 ||
           params->min_quantizers[layer] < 0 ||
@@ -4021,7 +4022,6 @@ static aom_codec_err_t ctrl_set_svc_params(aom_codec_alg_priv_t *ctx,
         return AOM_CODEC_INVALID_PARAM;
       }
     }
-    if (!av1_alloc_layer_context(cpi, num_layers)) return AOM_CODEC_MEM_ERROR;
 
     for (sl = 0; sl < ppi->number_spatial_layers; ++sl) {
       for (tl = 0; tl < ppi->number_temporal_layers; ++tl) {
