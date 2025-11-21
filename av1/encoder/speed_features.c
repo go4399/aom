cb6faf2358eef455948ddf38dcee20552084df33
Julio Barba <juliobbv@gmail.com>
2025-11-21 15:30:06 -0500

    Adaptive CDEF: make decoder-friendly at middle quality levels
    Make Adaptive CDEF more decoder-friendly in all intra mode by
    disabling CDEF on superblocks that were assigned low strength
    levels by the CDEF search algorithm.
    
    This change is implemented as a speed feature, enabled in all intra
    mode only: `lpf_sf.zero_low_cdef_strengths`.
    
    This reduces single-threaded image decoding overhead to ~10% (over
    `--enable-cdef 0`) on the CLIC 2020 dataset at speed 6, QP 30,
    with an overall 0.18 point loss in SSIMULACRA 2 scores (over the no
    decoder-friendly) implementation.
    
    Bug: aomedia:463698815
    Change-Id: I6a6307738cca5af12219b7def4bcf50551a535fa

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -2429,6 +2429,7 @@ static inline void init_lpf_sf(LOOP_FILTER_SPEED_FEATURES *lpf_sf) {
   lpf_sf->cdef_pick_method = CDEF_FULL_SEARCH;
   // Set decoder side speed feature to use less dual sgr modes
   lpf_sf->dual_sgr_penalty_level = 0;
+  lpf_sf->zero_low_cdef_strengths = 0;
   // Enable Wiener and Self-guided Loop restoration filters by default.
   lpf_sf->disable_wiener_filter = false;
   lpf_sf->disable_sgr_filter = false;
@@ -2799,6 +2800,12 @@ void av1_set_speed_features_qindex_dependent(AV1_COMP *cpi, int speed) {
               : cm->quant_params.base_qindex > qindex_thresh;
     }
     return;
+  }
+
+  if (cpi->oxcf.mode == ALLINTRA) {
+    if (cm->quant_params.base_qindex <= 140) {
+      sf->lpf_sf.zero_low_cdef_strengths = 1;
+    }
   }
 
   if (speed == 0) {
