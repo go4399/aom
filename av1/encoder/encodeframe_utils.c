a49f483af78dd50d2b737937e2375c4e996cb54c
Marco Paniconi <marpan@google.com>
2025-12-04 16:57:35 -0800

    Speculative fix for crash issue
    Crash happens for speed 6 realtime encoding,
    points to mi->bsize being invalid. For now try
    a fix of using bsize instead of mi->bsize for
    the mi_size_width/height[].
    
    The check for mi != NULL is done in parent functions
    where the pc_tree is allocated, so issue is likely
    that mi->bsize does not get set.
    
    Bug: b:457951958
    Change-Id: I80f3a3c1ea90d67e08017c3a4eaa13bb18ff811b

--- av1/encoder/encodeframe_utils.c
+++ av1/encoder/encodeframe_utils.c
@@ -186,8 +186,8 @@ void av1_update_state(const AV1_COMP *const cpi, ThreadData *td,
   const struct segmentation *const seg = &cm->seg;
   assert(bsize < BLOCK_SIZES_ALL);
   assert(mi != NULL && mi->bsize == bsize);
-  const int bw = mi_size_wide[mi->bsize];
-  const int bh = mi_size_high[mi->bsize];
+  const int bw = mi_size_wide[bsize];
+  const int bh = mi_size_high[bsize];
   const int mis = mi_params->mi_stride;
   const int mi_width = mi_size_wide[bsize];
   const int mi_height = mi_size_high[bsize];
