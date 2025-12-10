ca73305a91eb358cb95fec7affe77b22673f84c3
Julio Barba <juliobbv@gmail.com>
2025-12-10 18:16:02 -0500

    Adaptive CDEF: do not apply to chroma in CDEF_PICK_FROM_Q mode
    Testing revealed that in `CDEF_PICK_FROM_Q` mode (i.e. speeds >= 7
    in all intra mode), CDEF is applied to both luma and chroma channels
    throughout the entire image, significantly impacting decoding times.
    
    Because a single set of strengths are used to keep encoding
    computational complexity in check, a reasonable trade-off is to turn
    off CDEF for the chroma channels (U and V), reducing CDEF decode
    complexity overhead by up to 2/3 in 4:4:4 chroma subsampling mode.
    
    A visual check revealed chroma does ring a bit more for a few select
    images without CDEF, but the trade-off is worth it at these fast
    encode speeds.
    
    | Mode            | Total Decode Time | SSIMULACRA2 (QP30) |
    |-----------------|-------------------|--------------------|
    | CDEF off        | 4712.7 ms         | 74.28              |
    | Old adapt. CDEF | 5696.9 ms         | 74.34              |
    | New adapt. CDEF | 5050.9 ms         | 74.30              |
    
    Bug: aomedia:463698815
    Change-Id: Ia722e05de79c3603b4ae34c7a236b5d600cd8d94

--- av1/encoder/pickcdef.c
+++ av1/encoder/pickcdef.c
@@ -732,7 +732,7 @@ static inline void cdef_params_init(const YV12_BUFFER_CONFIG *frame,
 }
 
 void av1_pick_cdef_from_qp(AV1_COMMON *const cm, int skip_cdef,
-                           int is_screen_content) {
+                           int is_screen_content, bool avoid_uv_cdef) {
   const int bd = cm->seq_params->bit_depth;
   const int q =
       av1_ac_quant_QTX(cm->quant_params.base_qindex, 0, bd) >> (bd - 8);
@@ -796,7 +796,8 @@ void av1_pick_cdef_from_qp(AV1_COMMON *const cm, int skip_cdef,
   cdef_info->cdef_strengths[0] =
       predicted_y_f1 * CDEF_SEC_STRENGTHS + predicted_y_f2;
   cdef_info->cdef_uv_strengths[0] =
-      predicted_uv_f1 * CDEF_SEC_STRENGTHS + predicted_uv_f2;
+      avoid_uv_cdef ? 0
+                    : predicted_uv_f1 * CDEF_SEC_STRENGTHS + predicted_uv_f2;
 
   // mbmi->cdef_strength is already set in the encoding stage. We don't need to
   // set it again here.
@@ -844,7 +845,7 @@ void av1_cdef_search(AV1_COMP *cpi) {
   // Indicate if external RC is used for testing
   const int rtc_ext_rc = cpi->rc.rtc_external_ratectrl;
   if (rtc_ext_rc) {
-    av1_pick_cdef_from_qp(cm, 0, 0);
+    av1_pick_cdef_from_qp(cm, 0, 0, false);
     return;
   }
   CDEF_PICK_METHOD pick_method = cpi->sf.lpf_sf.cdef_pick_method;
@@ -854,8 +855,16 @@ void av1_cdef_search(AV1_COMP *cpi) {
             AOMMAX(cpi->sf.rt_sf.screen_content_cdef_filter_qindex_thresh,
                    cpi->rc.best_quality + 5) &&
         cpi->oxcf.tune_cfg.content == AOM_CONTENT_SCREEN;
+
+    // For adaptive CDEF, do not apply CDEF to chroma channels.
+    // This is done to reduce decode time, as CDEF is a relatively-expensive
+    // filter to compute.
+    const bool avoid_uv_cdef =
+        cdef_control == CDEF_ADAPTIVE && cpi->oxcf.mode == ALLINTRA &&
+        (cpi->oxcf.rc_cfg.mode == AOM_Q || cpi->oxcf.rc_cfg.mode == AOM_CQ);
+
     av1_pick_cdef_from_qp(cm, cpi->sf.rt_sf.skip_cdef_sb,
-                          use_screen_content_model);
+                          use_screen_content_model, avoid_uv_cdef);
     return;
   }
   const CommonModeInfoParams *const mi_params = &cm->mi_params;
