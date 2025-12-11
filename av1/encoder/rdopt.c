fd6b1adaf0819d400e4553de69c685fdbb424766
Marco Paniconi <marpan@google.com>
2025-12-11 15:21:39 -0800

    rtc: Speculative fix for crash issue:
    Change-Id: I5c7e116b94d1734690772505d524e33639ddd6b9

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -3582,6 +3582,13 @@ void av1_rd_pick_intra_mode_sb(const struct AV1_COMP *cpi, struct macroblock *x,
   int64_t dist_y = 0, dist_uv = 0;
 
   ctx->rd_stats.skip_txfm = 0;
+  if (cpi->sf.part_sf.partition_search_type == FIXED_PARTITION ||
+      cpi->sf.part_sf.partition_search_type == VAR_BASED_PARTITION) {
+    // In case there is early return due to rd_cost->rate == INT_MAX
+    // set the ctx->mic.bsize here. bsize is fixed and never changes for
+    // variance or fixed partition method.
+    ctx->mic.bsize = bsize;
+  }
   mbmi->ref_frame[0] = INTRA_FRAME;
   mbmi->ref_frame[1] = NONE_FRAME;
   mbmi->use_intrabc = 0;
