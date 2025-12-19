e66c678b691b583c918acab7be0f453e15452bb8
Marco Paniconi <marpan@google.com>
2025-12-19 08:56:45 -0800

    Placeholder for source analysis for one_pass_rt_lag
    For the one_pass realtime with lookahead encoding mode:
    add source analysis processing of frames in lookahead buffer,
    for adjusting the gop parameters.
    
    WIP
    
    Change-Id: I5d20b35848cf60d86e65364fc50a269029889b6f

--- av1/encoder/pass2_strategy.c
+++ av1/encoder/pass2_strategy.c
@@ -3808,6 +3808,20 @@ static void get_one_pass_rt_lag_params(AV1_COMP *cpi, unsigned int frame_flags,
             : MAX_GF_LENGTH_LAP;
     // Limit the max gop length for the last gop in 1 pass setting.
     max_gop_length = AOMMIN(max_gop_length, rc->frames_to_key);
+    // Go through source frames in lookahead buffer and compute source metrics:
+    // scene change, frame average source sad, etc.
+    for (int i = 1; i < max_gop_length; i++) {
+      EncodeFrameInput *frame_input;
+      memset(&frame_input, 0, sizeof(frame_input));
+      struct lookahead_entry *e =
+          av1_lookahead_peek(cpi->ppi->lookahead, i, cpi->compressor_stage);
+      struct lookahead_entry *e_prev =
+          av1_lookahead_peek(cpi->ppi->lookahead, i - 1, cpi->compressor_stage);
+      frame_input->source = &e->img;
+      frame_input->last_source = &e_prev->img;
+      rc->avg_source_sad = 0;
+      av1_rc_scene_detection_onepass_rt(cpi, frame_input);
+    }
     calculate_gf_length(cpi, max_gop_length, MAX_NUM_GF_INTERVALS);
     define_gf_group(cpi, frame_params, 0);
     rc->frames_till_gf_update_due = p_rc->baseline_gf_interval;
