a11ac3d057186b115c5d46980fe6ada1b10f12e7
ttwu <ttwu@andestech.com>
2026-02-02 15:44:35 +0800

    add high bit depth compound convolve optimization
    Change-Id: I63f2c40edf30418d45bc62cb7b952754d99b7f20

--- av1/av1.cmake
+++ av1/av1.cmake
@@ -481,7 +481,8 @@ list(APPEND AOM_AV1_COMMON_INTRIN_RVV
             "${AOM_ROOT}/av1/common/riscv/cdef_block_rvv.c"
             "${AOM_ROOT}/av1/common/riscv/convolve_rvv.c"
             "${AOM_ROOT}/av1/common/riscv/highbd_convolve_rvv.c"
-            "${AOM_ROOT}/av1/common/riscv/compound_convolve_rvv.c")
+            "${AOM_ROOT}/av1/common/riscv/compound_convolve_rvv.c"
+            "${AOM_ROOT}/av1/common/riscv/highbd_compound_convolve_rvv.c")
 
 if(CONFIG_THREE_PASS)
   list(APPEND AOM_AV1_ENCODER_SOURCES "${AOM_ROOT}/av1/encoder/thirdpass.c"
