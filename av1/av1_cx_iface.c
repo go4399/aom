c3ca82fe217999f361b481565980a5a6d8863d01
Narayan <narayan.kalaburgi@ittiam.com>
2026-03-04 17:34:26 +0530

    lc-dec: Enable low-complexity decode mode for hdres
    This patch enables low-complexity decode mode for HD resolution
    by extending the speed features of shorts.
    
    Performance results of encoder and decoder for the streams
    generated with low complexity decode enabled.
    
          Decode Time                   BD-Rate Loss(%)
    cpu  Reduction(%)  avg.psnr  ssim    vmaf    vmaf_neg  uvq_1p5
     1     19.057      1.3779    1.2776  1.3951  1.4730    1.6249
     2     17.989      1.2968    1.1682  1.4135  1.4562    1.1208
     3     16.925      1.2230    1.1199  1.3259  1.3151    1.7119
    
    STATS_CHANGED for low complexity decode, hdres
    
    Change-Id: Id755972989f1b62ff136590da6e2e2fa7dbb0086

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -1372,8 +1372,8 @@ static void set_encoder_config(AV1EncoderConfig *oxcf,
   // 608p and 720p. This can be further modified if needed.
   const int is_low_complexity_decode_mode_supported =
       (cfg->g_usage == AOM_USAGE_GOOD_QUALITY) &&
-      (oxcf->speed >= 1 && oxcf->speed <= 3) && (cfg->g_w < cfg->g_h) &&
-      (AOMMIN(cfg->g_w, cfg->g_h) >= 608 && AOMMIN(cfg->g_w, cfg->g_h) <= 1080);
+      (oxcf->speed >= 1 && oxcf->speed <= 3) &&
+      (AOMMIN(cfg->g_w, cfg->g_h) >= 608 && AOMMIN(cfg->g_w, cfg->g_h) < 2160);
   oxcf->enable_low_complexity_decode =
       extra_cfg->enable_low_complexity_decode &&
       is_low_complexity_decode_mode_supported;
