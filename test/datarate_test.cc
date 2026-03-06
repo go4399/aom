ec1ec3676851357992c1a59d57f9efe8171d97d0
Lin Zheng <linzhen@google.com>
2026-03-06 18:04:20 +0000

    Fix libaom nightly build in RTC
    Unit tests threshold tweaking under RTC+VBR config
    
    Change-Id: I2170bef666bcb1fcc4a9a3e68f9a617bf71daeb5

--- test/datarate_test.cc
+++ test/datarate_test.cc
@@ -373,8 +373,16 @@ class DatarateTestLarge
     ResetModel();
     lag_realtime_mode_ = 1;
     ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
+#if CONFIG_REALTIME_ONLY
+    // The theshold need to be lowered under CONFIG_REALTIME_ONLY due to the
+    // commit 2fed9c389d. If we set cfg_.g_lag_in_frames=0, the variation is
+    // smaller, and no need to lower the threshold by this much.
+    ASSERT_GE(effective_datarate_, cfg_.rc_target_bitrate * 0.65)
+        << " The datarate for the file is lower than target by too much!";
+#else
     ASSERT_GE(effective_datarate_, cfg_.rc_target_bitrate * 0.85)
         << " The datarate for the file is lower than target by too much!";
+#endif
     ASSERT_LE(effective_datarate_, cfg_.rc_target_bitrate * 2.0)
         << " The datarate for the file is greater than target by too much!";
   }
