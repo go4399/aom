875e4156eae57f117093c85f4cc1dd21f82c73f6
Julio Barba <juliobbv@gmail.com>
2026-03-10 13:05:10 -0400

    Optimize tune IQ and SSIMULACRA2 for inter-frame encoding
    - Adjust speed features for better image coding performance
    - Bias encoder toward picking intra-coded block candidates
    
    Approximate gains in good-quality mode, cpu-used=6, on two-layer
    progressive image encoding in 4:4:4 chroma subsampling mode
    (Daala's subset1):
    - SSIMULACRA 2 60: -2.1%
    - SSIMULACRA 2 70: -3.1%
    - SSIMULACRA 2 80: -3.2%
    - SSIMULACRA 2 90: -3.2%
    
    Change-Id: I7d88835ac993a11f74d7644be501ed0b56d7ba9a

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1068,6 +1068,12 @@ static void set_good_speed_feature_framesize_dependent(
 
   if (cpi->oxcf.enable_low_complexity_decode)
     set_good_speed_features_lc_dec_framesize_dependent(cpi, sf, speed);
+
+  // Speed features for the IQ and SSIMULACRA2 tuning modes
+  if (cpi->oxcf.tune_cfg.tuning == AOM_TUNE_IQ ||
+      cpi->oxcf.tune_cfg.tuning == AOM_TUNE_SSIMULACRA2) {
+    sf->intra_sf.skip_intra_in_interframe = 0;
+  }
 }
 
 static void set_good_speed_features_framesize_independent(
@@ -1479,6 +1485,23 @@ static void set_good_speed_features_framesize_independent(
   if (cpi->oxcf.algo_cfg.sharpness == 3) {
     sf->tx_sf.adaptive_txb_search_level = 0;
     sf->tx_sf.tx_type_search.use_skip_flag_prediction = 0;
+  }
+
+  // Speed features for the IQ and SSIMULACRA2 tuning modes
+  if (cpi->oxcf.tune_cfg.tuning == AOM_TUNE_IQ ||
+      cpi->oxcf.tune_cfg.tuning == AOM_TUNE_SSIMULACRA2) {
+    sf->intra_sf.skip_intra_in_interframe = 0;
+    sf->inter_sf.inter_mode_rd_model_estimation = 0;
+    sf->mv_sf.use_intrabc = 1;
+
+    // Don't prune intra candidates too aggressively, as it can cause more
+    // expensive inter candidates to be chosen instead
+    if (sf->intra_sf.intra_pruning_with_hog > 3) {
+      sf->intra_sf.intra_pruning_with_hog = 3;
+    }
+    if (sf->intra_sf.chroma_intra_pruning_with_hog > 3) {
+      sf->intra_sf.chroma_intra_pruning_with_hog = 3;
+    }
   }
 }
 
@@ -2164,6 +2187,12 @@ static void set_rt_speed_features_framesize_independent(AV1_COMP *cpi,
   if (speed >= 11 && !frame_is_intra_only(cm) &&
       cpi->oxcf.tune_cfg.content == AOM_CONTENT_SCREEN) {
     sf->winner_mode_sf.dc_blk_pred_level = 3;
+  }
+
+  // Speed features for the IQ and SSIMULACRA2 tuning modes
+  if (cpi->oxcf.tune_cfg.tuning == AOM_TUNE_IQ ||
+      cpi->oxcf.tune_cfg.tuning == AOM_TUNE_SSIMULACRA2) {
+    sf->intra_sf.skip_intra_in_interframe = 0;
   }
 }
 
