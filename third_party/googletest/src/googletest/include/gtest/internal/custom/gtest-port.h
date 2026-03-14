188ea954949267236999003b5f029f0167d5e72e
Wan-Teh Chang <wtc@google.com>
2026-03-13 20:29:40 -0700

    Update to GoogleTest 1.17.0
    Change-Id: I28ab91d3c1deb3fb5dc97d9435a1c5dad2daba2c

--- third_party/googletest/src/googletest/include/gtest/internal/custom/gtest-port.h
+++ third_party/googletest/src/googletest/include/gtest/internal/custom/gtest-port.h
@@ -36,7 +36,7 @@
 
 // Use a stub Notification class.
 //
-// The built-in Notification class in GoogleTest v1.12.1 uses std::mutex and
+// The built-in Notification class in GoogleTest uses std::mutex and
 // std::condition_variable. The <mutex> and <condition_variable> headers of
 // mingw32 g++ (GNU 10.0.0) define std::mutex and std::condition_variable only
 // when configured with the posix threads option but don't define them when
@@ -47,20 +47,26 @@
 // Notification class using Windows CRITICAL_SECTION and CONDITION_VARIABLE,
 // it's simpler to just use a stub Notification class on all platforms.
 //
-// The default constructor of the stub class is deleted and the declaration of
-// the Notify() method is commented out, so that compilation will fail if any
-// code actually uses the Notification class.
+// The Notify() and WaitForNotification() methods dereference a null pointer,
+// so that the process will crash if any code actually uses the Notification
+// class.
 
 #define GTEST_HAS_NOTIFICATION_ 1
 namespace testing {
 namespace internal {
 class Notification {
  public:
-  Notification() = delete;
+  Notification() = default;
   Notification(const Notification&) = delete;
   Notification& operator=(const Notification&) = delete;
-  // void Notify();
-  void WaitForNotification() {}
+  void Notify() {
+    int* p = nullptr;
+    *p = 0;
+  }
+  void WaitForNotification() {
+    int* p = nullptr;
+    *p = 0;
+  }
 };
 }  // namespace internal
 }  // namespace testing
