4e311222207fa2e46eff4cb22d2173e94c88cc7f
James Zern <jzern@google.com>
2026-03-14 18:21:45 -0700

    av1 encoder: reject unequal UV strides
    The `aom_image_t` from the external API is converted to an internal
    `YV12_BUFFER_CONFIG`, which assumes U and V strides are the same. This
    avoids a potential crash or corrupted chroma if they differ.
    
    Ported from libvpx:
      915643b94 vp[89] encoders: reject unequal UV strides
    
    Bug: 492213293
    Change-Id: I3c878bdd330a8f7f838ac3bce8b66201c49f537d

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -965,6 +965,8 @@ static aom_codec_err_t validate_img(aom_codec_alg_priv_t *ctx,
     case AOM_IMG_FMT_YV12:
     case AOM_IMG_FMT_NV12:
     case AOM_IMG_FMT_I420:
+    case AOM_IMG_FMT_AOMYV12:
+    case AOM_IMG_FMT_AOMI420:
     case AOM_IMG_FMT_YV1216:
     case AOM_IMG_FMT_I42016: break;
     case AOM_IMG_FMT_I444:
@@ -989,6 +991,10 @@ static aom_codec_err_t validate_img(aom_codec_alg_priv_t *ctx,
 
   if (img->d_w != ctx->cfg.g_w || img->d_h != ctx->cfg.g_h)
     ERROR("Image size must match encoder init configuration size");
+  assert(img->fmt & AOM_IMG_FMT_PLANAR);
+  if (!ctx->cfg.monochrome && img->fmt != AOM_IMG_FMT_NV12 &&
+      img->stride[AOM_PLANE_U] != img->stride[AOM_PLANE_V])
+    ERROR("Image U/V strides must match");
 
   // 6.4.2 Color config semantics
   // If matrix_coefficients is equal to MC_IDENTITY, it is a requirement of
@@ -1020,7 +1026,9 @@ static int get_image_bps(const aom_image_t *img) {
   switch (img->fmt) {
     case AOM_IMG_FMT_YV12:
     case AOM_IMG_FMT_NV12:
-    case AOM_IMG_FMT_I420: return 12;
+    case AOM_IMG_FMT_I420:
+    case AOM_IMG_FMT_AOMYV12:
+    case AOM_IMG_FMT_AOMI420: return 12;
     case AOM_IMG_FMT_I422: return 16;
     case AOM_IMG_FMT_I444: return 24;
     case AOM_IMG_FMT_YV1216:
