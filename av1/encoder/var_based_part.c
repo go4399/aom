9f12179cb1bc7e8562124d15afabac0dd059bc2d
Marco Paniconi <marpan@google.com>
2026-03-19 20:30:13 -0700

    rtc: Rework vertical search window for scroll sb motion
    For superblock motion in variance partitioning: allow for
    larger vertical search for scroll motion detection.
    
    This helps to reduce encode_time spikes, observed in
    4K webrtc screen content test with scrolling.
    
    Change-Id: I4a452a586ae9017808153af9540b7ce6682c793d

--- av1/encoder/var_based_part.c
+++ av1/encoder/var_based_part.c
@@ -1360,7 +1360,7 @@ static void do_int_pro_motion_estimation(AV1_COMP *cpi, MACROBLOCK *x,
   unsigned int y_sad_zero;
   *y_sad = av1_int_pro_motion_estimation(
       cpi, x, cm->seq_params->sb_size, mi_row, mi_col, &kZeroMv, &y_sad_zero,
-      me_search_size_col, me_search_size_row);
+      me_search_size_col, me_search_size_row, 1);
   // The logic below selects whether the motion estimated in the
   // int_pro_motion() will be used in nonrd_pickmode. Only do this
   // for screen for now.
