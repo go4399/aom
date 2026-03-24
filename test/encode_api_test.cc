3cfc26e71128052d72641d8db08a9884698706eb
James Zern <jzern@google.com>
2026-03-24 14:09:13 -0700

    encode_api_test.cc: fix Visual Studio warnings
    Use a ternary rather than the result of a comparison to set the maximum
    monochrome value in a loop.
    
    After:
      a749f280b3 av1 encoder: reject unequal UV strides
    
    Fixes:
    ```
      test\encode_api_test.cc(240,33):
    	warning C4804: '<=': unsafe use of type 'bool' in operation
      test\encode_api_test.cc(240,45):
        warning C4018: '<=': signed/unsigned mismatch
    ```
    
    Bug: 492213293
    Change-Id: I950ce67da5969d9efbab5963520a457c6ab3f6fb

--- test/encode_api_test.cc
+++ test/encode_api_test.cc
@@ -237,7 +237,7 @@ TEST(EncodeAPI, InvalidUVStrides) {
     cfg.g_h = kHeight;
     cfg.g_bit_depth = high_bit_depth ? AOM_BITS_10 : AOM_BITS_8;
     // Monochrome is not allowed in profile 1.
-    for (cfg.monochrome = 0; cfg.monochrome <= (cfg.g_profile != 1);
+    for (cfg.monochrome = 0; cfg.monochrome <= (cfg.g_profile == 1 ? 0u : 1u);
          ++cfg.monochrome) {
       ASSERT_EQ(
           aom_codec_enc_init(&enc, aom_codec_av1_cx(), &cfg,
