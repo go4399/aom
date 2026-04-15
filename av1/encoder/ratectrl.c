95b19d1cf6686fc726e3e1c1bf80d07f359269a6
Marco Paniconi <marpan@google.com>
2026-04-15 15:13:08 -0700

    Add check to validate allocation size of src_sad_blk_64x64
    Fixes heap buffer overflow in dynamic svc.
    
    Bug: 502735235
    Change-Id: I52e52c8b14a25f453a62d74575ea1cd8c56a3e72

--- av1/encoder/ratectrl.c
+++ av1/encoder/ratectrl.c
@@ -3360,10 +3360,16 @@ void av1_rc_scene_detection_onepass_rt(AV1_COMP *cpi,
   // Store blkwise SAD for later use. Disable for spatial layers for now.
   if (width == cm->render_width && height == cm->render_height &&
       cpi->svc.number_spatial_layers == 1 && !is_one_pass_rt_lag_params(cpi)) {
+    if (cpi->src_sad_blk_64x64 != NULL &&
+        cpi->src_sad_blk_alloc_size != sb_cols * sb_rows) {
+      aom_free(cpi->src_sad_blk_64x64);
+      cpi->src_sad_blk_64x64 = NULL;
+    }
     if (cpi->src_sad_blk_64x64 == NULL) {
       CHECK_MEM_ERROR(cm, cpi->src_sad_blk_64x64,
                       (uint64_t *)aom_calloc(sb_cols * sb_rows,
                                              sizeof(*cpi->src_sad_blk_64x64)));
+      cpi->src_sad_blk_alloc_size = sb_cols * sb_rows;
     }
   }
   const CommonModeInfoParams *const mi_params = &cpi->common.mi_params;
