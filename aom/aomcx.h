89b41edd10bde187de1017359ac40037a660f547
Marco Paniconi <marpan@google.com>
2026-04-19 20:41:07 -0700

    svc: Check for invalid params for svc layer id setting
    Return AOM_CODEC_INVALID_PARAM if the spatial or
    temporal layer id is set to be below 0 or above
    the configured setting. Update the documentation
    of the AV1E_SET_SVC_LAYER_ID control.
    
    Bug: 503975732
    Change-Id: Idb30133e116bddb8ac869bb7dcf37a4b99c12f64

--- aom/aomcx.h
+++ aom/aomcx.h
@@ -1289,7 +1289,9 @@ enum aome_enc_control_id {
   /* NOTE: enums 145-149 unused */
 
   /*!\brief Codec control function to set the layer id, aom_svc_layer_id_t*
-   * parameter
+   * parameter. Layer id for spatial or temporal layer must be within valid
+   * range of 0 to the allowed number of spatial or temporal layers, set via
+   * the control AV1E_SET_SVC_PARAMS, or via AOME_SET_SPATIAL_LAYER_ID.
    */
   AV1E_SET_SVC_LAYER_ID = 131,
 
--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -4064,6 +4064,10 @@ static aom_codec_err_t ctrl_set_number_spatial_layers(aom_codec_alg_priv_t *ctx,
 static aom_codec_err_t ctrl_set_layer_id(aom_codec_alg_priv_t *ctx,
                                          va_list args) {
   aom_svc_layer_id_t *const data = va_arg(args, aom_svc_layer_id_t *);
+  if (data->spatial_layer_id < 0 || data->temporal_layer_id < 0 ||
+      data->spatial_layer_id >= ctx->ppi->cpi->svc.number_spatial_layers ||
+      data->temporal_layer_id >= ctx->ppi->cpi->svc.number_temporal_layers)
+    return AOM_CODEC_INVALID_PARAM;
   ctx->ppi->cpi->common.spatial_layer_id = data->spatial_layer_id;
   ctx->ppi->cpi->common.temporal_layer_id = data->temporal_layer_id;
   ctx->ppi->cpi->svc.spatial_layer_id = data->spatial_layer_id;
