267019beaae10e2ff251a6a1c7db774598f779f0
Narayan Kalaburgi <narayan.kalaburgi@ittiam.com>
2026-04-27 12:05:49 +0530

    lc-dec: Enable sf skip_loop_filter_using_filt_error
    This CL enables the speed feature
    'skip_loop_filter_using_filt_error' for shorts and hdres in
    lc decode mode. This speed feature remains disabled when
    frame type is OVERLAY_UPDATE or INTNL_OVERLAY_UPDATE or
    current frame pyramid_level <= 1.
    
    STATS_CHANGED for low complexity decode
    
    Change-Id: I55655cd0908abc75b60a1196eca84a9599a3ca98

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -658,6 +658,11 @@ static void set_good_speed_features_lc_dec_framesize_dependent(
 
     sf->lpf_sf.dual_sgr_penalty_level = boosted ? 1 : 3;
     sf->lpf_sf.switchable_lr_with_bias_level = 1;
+    sf->lpf_sf.skip_loop_filter_using_filt_error =
+        (update_type != OVERLAY_UPDATE && update_type != INTNL_OVERLAY_UPDATE &&
+         cm->current_frame.pyramid_level > 1)
+            ? 1
+            : 0;
 
     sf->inter_sf.bias_warp_mode_rd_scale_pct = 4;
     sf->inter_sf.bias_obmc_mode_rd_scale_pct = 1.5f;
@@ -674,6 +679,12 @@ static void set_good_speed_features_lc_dec_framesize_dependent(
     sf->gm_sf.gm_erroradv_tr_level = 1;
 
     sf->hl_sf.ref_frame_mvs_lvl = 1;
+
+    sf->lpf_sf.skip_loop_filter_using_filt_error =
+        (update_type != OVERLAY_UPDATE && update_type != INTNL_OVERLAY_UPDATE &&
+         cm->current_frame.pyramid_level > 1)
+            ? 1
+            : 0;
 
     sf->inter_sf.bias_warp_mode_rd_scale_pct = 4;
     sf->inter_sf.bias_obmc_mode_rd_scale_pct = 1.5f;
