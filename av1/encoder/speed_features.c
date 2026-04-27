076dc53cc724fe692fbd42810c6567cda92f1091
Mudassir Galaganath <mudassir.galaganath@ittiam.com>
2026-04-27 22:33:18 +0530

    lc-dec: Add sf to disable CDEF adaptively
    This patch adds speed feature 'adaptive_cdef_mode' that always
    disables chroma CDEF, and disables luma CDEF when the MSE
    improvement of a frame over zero filter strength falls below
    a threshold.
    
    Performance results of encoder and decoder for streams generated
    with low complexity decode enabled, measured w.r.t a baseline with
    CDEF disabled (--enable-cdef=0).
    
          Decode Time                   BD-Rate Loss(%)
    cpu  Reduction(%)  avg.psnr   ssim    vmaf    vmaf_neg  uvq_1p5
     1     -4.048     -0.3578   -0.4008  -0.5414  -0.3696   -8.6267
     2     -4.164     -0.4334   -0.5844  -0.5302  -0.3774  -10.3880
     3     -4.716     -0.6492   -0.8754  -0.7096  -0.5201  -10.9057
    
    STATS_CHANGED for low complexity decode
    
    Change-Id: I5e21281a5bb8f97c748cb37a9169690a02c45885

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -656,6 +656,7 @@ static void set_good_speed_features_lc_dec_framesize_dependent(
 
     sf->hl_sf.ref_frame_mvs_lvl = 2;
 
+    sf->lpf_sf.adaptive_cdef_mode = 1;
     sf->lpf_sf.dual_sgr_penalty_level = boosted ? 1 : 3;
     sf->lpf_sf.switchable_lr_with_bias_level = 1;
     sf->lpf_sf.skip_loop_filter_using_filt_error =
@@ -680,6 +681,7 @@ static void set_good_speed_features_lc_dec_framesize_dependent(
 
     sf->hl_sf.ref_frame_mvs_lvl = 1;
 
+    sf->lpf_sf.adaptive_cdef_mode = 1;
     sf->lpf_sf.skip_loop_filter_using_filt_error =
         (update_type != OVERLAY_UPDATE && update_type != INTNL_OVERLAY_UPDATE &&
          cm->current_frame.pyramid_level > 1)
@@ -2530,6 +2532,7 @@ static inline void init_lpf_sf(LOOP_FILTER_SPEED_FEATURES *lpf_sf) {
   lpf_sf->disable_wiener_coeff_refine_search = false;
   lpf_sf->use_downsampled_wiener_stats = 0;
   lpf_sf->switchable_lr_with_bias_level = 0;
+  lpf_sf->adaptive_cdef_mode = 0;
 }
 
 static inline void init_rt_sf(REAL_TIME_SPEED_FEATURES *rt_sf) {
