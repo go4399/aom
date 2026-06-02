bff728111b0657e08c32e1878f2bfc07eb3fa3bc
Jerome Jiang <jianj@google.com>
2026-06-02 17:42:03 -0400

    Replace native AVX2 convolve horiz with Highway
    Map the Highway convolve implementation directly under the
    native `_avx2` suffix when CONFIG_HIGHWAY is enabled, replacing
    the hand-written legacy AVX2 assembly convolve.
    
    Fallback to native SSSE3 assembly for the 16x32 block size
    
    Size    | Leg AVX2 | Hwy AVX2 | Hwy AV512 | AVX2 % | AV512 %
    --------+----------+----------+-----------+--------+--------
    4x4     | 5.25µs   | 4.63µs   | 4.43µs    | +11.8% | +15.6%
    8x4     | 5.32µs   | 4.62µs   | 4.42µs    | +13.1% | +16.9%
    4x8     | 7.17µs   | 5.77µs   | 5.58µs    | +19.5% | +22.1%
    8x8     | 6.62µs   | 5.77µs   | 5.57µs    | +12.8% | +15.8%
    16x8    | 7.49µs   | 7.07µs   | 6.71µs    | +5.6%  | +10.4%
    8x16    | 9.64µs   | 8.23µs   | 8.05µs    | +14.6% | +16.4%
    16x16   | 11.52µs  | 11.03µs  | 10.86µs   | +4.2%  | +5.7%
    32x16   | 20.93µs  | 19.79µs  | 19.84µs   | +5.4%  | +5.2%
    16x32   | 19.44µs  | 20.54µs  | 20.73µs   | -5.6%  | -6.6%
    32x32   | 36.92µs  | 33.54µs  | 33.41µs   | +9.1%  | +9.5%
    64x32   | 73.25µs  | 59.77µs  | 59.09µs   | +18.4% | +19.3%
    32x64   | 171.2µs  | 152.6µs  | 153.6µs   | +10.8% | +10.2%
    64x64   | 307.8µs  | 254.9µs  | 152.3µs   | +17.1% | +50.5%
    128x64  | 527.4µs  | 466.7µs  | 299.9µs   | +11.5% | +43.1%
    64x128  | 679.3µs  | 505.2µs  | 306.1µs   | +25.6% | +54.9%
    128x128 | 1.355ms  | 937.8µs  | 596.6µs   | +30.8% | +55.9%
    
    Change-Id: I5956f2eee277310e7aeb4b28ab4c2e496686daa6

--- aom_dsp/x86/aom_subpixel_8t_intrin_avx2.c
+++ aom_dsp/x86/aom_subpixel_8t_intrin_avx2.c
@@ -63,6 +63,7 @@ static inline void xx_store2_mi128(const uint8_t *output_ptr,
                   _mm256_extractf128_si256(*a, 1));
 }
 
+#if !CONFIG_HIGHWAY
 static void aom_filter_block1d4_h4_avx2(
     const uint8_t *src_ptr, ptrdiff_t src_pixels_per_line, uint8_t *output_ptr,
     ptrdiff_t output_pitch, uint32_t output_height, const int16_t *filter) {
@@ -784,8 +785,6 @@ static void aom_filter_block1d16_h8_avx2(
   }
 }
 
-#if !CONFIG_HIGHWAY
-
 static inline __m256i xx_loadu2_epi64(const void *hi, const void *lo) {
   __m256i a = _mm256_castsi128_si256(_mm_loadl_epi64((const __m128i *)(lo)));
   a = _mm256_inserti128_si256(a, _mm_loadl_epi64((const __m128i *)(hi)), 1);
@@ -1405,6 +1404,7 @@ static void aom_filter_block1d4_v4_avx2(
 #endif  // !CONFIG_HIGHWAY
 
 #if HAVE_AVX2 && HAVE_SSSE3
+#if !CONFIG_HIGHWAY
 filter8_1dfunction aom_filter_block1d16_h2_ssse3;
 filter8_1dfunction aom_filter_block1d8_h2_ssse3;
 filter8_1dfunction aom_filter_block1d4_h2_ssse3;
@@ -1419,7 +1419,6 @@ filter8_1dfunction aom_filter_block1d4_h2_ssse3;
 //                                int w, int h);
 FUN_CONV_1D(horiz, x_step_q4, filter_x, h, src, , avx2)
 
-#if !CONFIG_HIGHWAY
 filter8_1dfunction aom_filter_block1d4_v8_ssse3;
 filter8_1dfunction aom_filter_block1d16_v2_ssse3;
 filter8_1dfunction aom_filter_block1d8_v2_ssse3;
@@ -1437,4 +1436,4 @@ filter8_1dfunction aom_filter_block1d4_v2_ssse3;
 FUN_CONV_1D(vert, y_step_q4, filter_y, v, src - src_stride * 3, , avx2)
 #endif  // !CONFIG_HIGHWAY
 
-#endif  // HAVE_AX2 && HAVE_SSSE3
+#endif  // HAVE_AVX2 && HAVE_SSSE3
