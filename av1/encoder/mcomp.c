846e783c125add981ecb5a1f39b2aae68dd5e847
Gyuyoung Kim <qkim@google.com>
2026-06-09 03:18:33 +0000

    [M144-LTS] av1_int_pro_motion_estimation: fix stride w/scaled ref
    `ref_stride` was set to `xd->plane[0].pre[0].stride` prior to the call
    to `vp9_setup_pre_planes()` which is used to update that entry when
    there is a scaled reference frame.
    
    This has been incorrect since:
     96dba4902 Fix integral projection motion search for frame resize
    
    Bug: 504613867
    Change-Id: I8f84be86d72b80e628ec96fc7620e650c6497639

--- av1/encoder/mcomp.c
+++ av1/encoder/mcomp.c
@@ -2152,7 +2152,6 @@ unsigned int av1_int_pro_motion_estimation(const AV1_COMP *cpi, MACROBLOCK *x,
       ((mi_row << 2) + search_size_height > cm->height + border))
     search_size_height = border;
   const int src_stride = x->plane[0].src.stride;
-  const int ref_stride = xd->plane[0].pre[0].stride;
   uint8_t const *ref_buf, *src_buf;
   int_mv *best_int_mv = &xd->mi[0]->mv[0];
   unsigned int best_sad, tmp_sad, this_sad[4];
@@ -2177,6 +2176,7 @@ unsigned int av1_int_pro_motion_estimation(const AV1_COMP *cpi, MACROBLOCK *x,
     av1_setup_pre_planes(xd, 0, scaled_ref_frame, mi_row, mi_col, NULL,
                          MAX_MB_PLANE);
   }
+  const int ref_stride = xd->plane[0].pre[0].stride;
 
   if (xd->bd != 8) {
     best_int_mv->as_fullmv = kZeroFullMv;
