7636265fc89b86cfc5f6a61c50b52fb8ab977bab
Wan-Teh Chang <wtc@google.com>
2026-07-08 12:59:20 -0700

    sum_squares_sve: Call funcs under right conditions
    Some functions in sum_squares_sve.c require `height` to be an even
    number. Some also work when `width` is not only 8 or 16 but also a
    multiple of 8 or 16. Call such functions under the right conditions.
    
    Bug: 527078408
    Change-Id: Ie4c31c2a41aac964e7d8807ece69bad3550b5312

--- test/sse_sum_test.cc
+++ test/sse_sum_test.cc
@@ -50,7 +50,7 @@ class SumSSETest : public ::testing::TestWithParam<TestFuncs> {
   }
 
   void TearDown() override { aom_free(src_); }
-  void RunTest(int isRandom);
+  void RunTest(int isRandom, int multipleOf4);
   void RunSpeedTest();
 
   void GenRandomData(int width, int height, int stride) {
@@ -82,12 +82,14 @@ class SumSSETest : public ::testing::TestWithParam<TestFuncs> {
 
 GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SumSSETest);
 
-void SumSSETest::RunTest(int isRandom) {
+void SumSSETest::RunTest(int isRandom, int multipleOf4) {
   for (int k = 0; k < kNumIterations; k++) {
-    const int width = 4 * (rnd_(31) + 1);   // Up to 128x128
-    const int height = 4 * (rnd_(31) + 1);  // Up to 128x128
-    int stride = 4 << rnd_(7);              // Up to 256 stride
-    while (stride < width) {                // Make sure it's valid
+    // Up to 128x128
+    const int width = multipleOf4 ? 4 * (rnd_(31) + 1) : rnd_(127) + 1;
+    // Up to 128x128
+    const int height = multipleOf4 ? 4 * (rnd_(31) + 1) : rnd_(127) + 1;
+    int stride = 4 << rnd_(7);  // Up to 256 stride
+    while (stride < width) {    // Make sure it's valid
       stride = 4 << rnd_(7);
     }
     if (isRandom) {
@@ -145,11 +147,16 @@ void SumSSETest::RunSpeedTest() {
 }
 
 TEST_P(SumSSETest, OperationCheck) {
-  RunTest(1);  // GenRandomData
+  RunTest(1, 1);  // GenRandomData, width and height multiple of 4
 }
 
 TEST_P(SumSSETest, ExtremeValues) {
-  RunTest(0);  // GenExtremeData
+  RunTest(0, 1);  // GenExtremeData, width and height multiple of 4
+}
+
+TEST_P(SumSSETest, ArbitraryWidthAndHeight) {
+  // GenRandomData, width and height not necessarily multiple of 4
+  RunTest(1, 0);
 }
 
 TEST_P(SumSSETest, DISABLED_Speed) { RunSpeedTest(); }
